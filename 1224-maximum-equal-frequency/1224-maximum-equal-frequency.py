9e093cabb1f64b4b90195078577f7c4566146a99
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 15:53:15 +0700

    Time: 254 ms (9.26%), Space: 25.3 MB (22.22%) - LeetHub

--- 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
@@ -1,25 +1,26 @@
 class Solution:
     def maxEqualFreq(self, nums: List[int]) -> int:
-        freq = Counter(nums)
+        freq = Counter()
+        cnt = Counter()
+        res = 0
 
-        def valid():
-            cnt = Counter(freq.values())
-            n = len(cnt)
-            min_key, max_key = min(cnt.keys()), max(cnt.keys())
-            return (
-                    n == 1 and (min_key == 1 or min(cnt.values()) == 1)
-                    or n == 2 and (
-                            1 in cnt and cnt[1] == 1
-                            or max_key - min_key == 1 and cnt[max_key] == 1
-                    )
-            )
+        for i, x in enumerate(nums):
+            f = freq[x]
+            if f:
+                cnt[f] -= 1
+                if cnt[f] == 0:
+                    del cnt[f]
 
-        for i in range(len(nums) - 1, -1, -1):
-            if valid():
-                return i + 1
-            if freq[nums[i]] == 1:
-                freq.pop(nums[i])
-            else:
-                freq[nums[i]] -= 1
+            freq[x] += 1
+            cnt[freq[x]] += 1
 
-        return 1
+            if (
+                len(cnt) == 1 and (1 in cnt or cnt[next(iter(cnt))] == 1)
+                or len(cnt) == 2 and (
+                    1 in cnt and cnt[1] == 1
+                    or max(cnt) == min(cnt) + 1 and cnt[max(cnt)] == 1
+                )
+            ):
+                res = i + 1
+
+        return res
