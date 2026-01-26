1553000bafe1a46c2f9a6a9b8ecbc7083fc17566
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 21:30:22 +0700

    Time: 6272 ms (11.86%), Space: 20.1 MB (16.86%) - LeetHub

--- 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.py
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.py
@@ -1,24 +1,23 @@
 class Solution:
     def subsequenceSumAfterCapping(self, nums: List[int], k: int) -> List[bool]:
-        n = len(nums)
-        freq = [0] * (max(nums) + 1)
+        n, m = len(nums), max(nums) + 1
+        freq = [0] * m
         for num in nums:
             freq[num] += 1
         res = []
         seen = [True] + [False] * k
-        cnt = 0
 
         def is_possible():
             for s in range(k + 1):
-                if seen[s] and (k - s) % x == 0 and (k - s) // x <= n - cnt:
+                if seen[s] and (k - s) % x == 0 and (k - s) // x <= n:
                     return True
             return False
 
         for x in range(1, n + 1):
-            while x < len(freq) and freq[x]:
+            while x < m and freq[x]:
                 for num in range(k, x - 1, -1):
                     seen[num] |= seen[num - x]
                 freq[x] -= 1
-                cnt += 1
+                n -= 1
             res.append(is_possible())
         return res
