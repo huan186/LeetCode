ee2dd2a935a777ce1c56e9140ea67153e2ae7c80
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 22:49:43 +0700

    Time: 0 ms (100%), Space: 19.3 MB (20.65%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -2,11 +2,14 @@ class Solution:
     def binaryGap(self, n: int) -> int:
         if n & n - 1 == 0:
             return 0
+        while n & 1 == 0:
+            n >>= 1
         res, cnt = 0, 0
-        for ch in str(bin(n)[2:]):
-            if ch == '1':
+        while n > 0:
+            if n & 1 == 1:
                 res = max(res, cnt)
                 cnt = 1
             else:
                 cnt += 1
+            n >>= 1
         return res
