790493b34626c95d24e3bb5c4a3882cdbb7af4b0
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 11:46:56 +0700

    Time: 65 ms (68.47%), Space: 22.1 MB (5.42%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -13,27 +13,25 @@ class Solution:
             (0, 4, 6), (1, 3, 5, 7), (2, 4, 8),
             (3, 7), (4, 6, 8), (5, 7)
         ]
-        pow10 = [10 ** (8 - i) for i in range(9)]
-        target = 111_111_111
+        target = sum(1 << 4 * i for i in range(9))
         q = deque([target])
         dist = {target: 0}
         while q:
             v = q.popleft()
             step = dist[v]
             for i in range(9):
-                if (v // pow10[i]) % 10 == 0:
+                if (v >> 4 * i) & 0xF == 0x0:
                     continue
                 for j in adj[i]:
-                    nxt = v - pow10[i] + pow10[j]
+                    nxt = v - (1 << 4 * i) + (1 << 4 * j)
                     if nxt not in dist:
                         dist[nxt] = step + 1
                         q.append(nxt)
         return dist
 
-
     def minimumMoves(self, grid: List[List[int]]) -> int:
         start = 0
         for r in grid:
             for x in r:
-                start = start * 10 + x
+                start = (start << 4) + x
         return Solution.dist[start]
