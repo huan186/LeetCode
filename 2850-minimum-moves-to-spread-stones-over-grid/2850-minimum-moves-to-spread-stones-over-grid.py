34d31710b6da1ed9d89b4e7fc8fbce55c678dd0a
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 14:12:20 +0700

    Time: 11 ms (91.13%), Space: 20.3 MB (17.73%) - LeetHub

--- 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -1,39 +1,30 @@
 class Solution:
-    dist = None
-
-    def __init__(self):
-        if Solution.dist is None:
-            Solution.dist = self.precompute()
-
-    @staticmethod
-    def precompute():
-        from collections import deque
-        adj = [
-            (1, 3), (0, 2, 4), (1, 5),
-            (0, 4, 6), (1, 3, 5, 7), (2, 4, 8),
-            (3, 7), (4, 6, 8), (5, 7)
-        ]
-        pow10 = [10 ** (8 - i) for i in range(9)]
-        target = 111_111_111
-        q = deque([target])
-        dist = {target: 0}
-        while q:
-            v = q.popleft()
-            step = dist[v]
-            for i in range(9):
-                if (v // pow10[i]) % 10 == 0:
-                    continue
-                for j in adj[i]:
-                    nxt = v - pow10[i] + pow10[j]
-                    if nxt not in dist:
-                        dist[nxt] = step + 1
-                        q.append(nxt)
-        return dist
-
+    # def __init__(self):
+    #     self.ans = None
 
     def minimumMoves(self, grid: List[List[int]]) -> int:
-        start = 0
-        for r in grid:
-            for x in r:
-                start = start * 10 + x
-        return Solution.dist[start]
+        zeros = []
+        extras = []
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == 0:
+                    zeros.append((i, j))
+                else:
+                    extras.append([i, j, grid[i][j] - 1])
+        self.ans = math.inf
+        def dfs(k, cost):
+            if cost >= self.ans:
+                return
+            if k == len(zeros):
+                self.ans = min(self.ans, cost)
+                return
+            zi, zj = zeros[k]
+            for e in extras:
+                if e[2] == 0:
+                    continue
+                e[2] -= 1
+                dfs(k + 1, cost + abs(zi - e[0]) + abs(zj - e[1]))
+                e[2] += 1
+                
+        dfs(0, 0)
+        return self.ans
