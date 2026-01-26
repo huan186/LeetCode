72787f6c62a6ab56b09c200955f3210914ed0e9f
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 21:36:43 +0700

    Time: 6365 ms (11.72%), Space: 19.9 MB (38.08%) - LeetHub

--- 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.py
+++ 3685-subsequence-sum-after-capping-elements/3685-subsequence-sum-after-capping-elements.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsequenceSumAfterCapping(self, nums: List[int], k: int) -> List[bool]:
-        n, m = len(nums), max(nums) + 1
+        n, m = len(nums), 4001
         freq = [0] * m
         for num in nums:
             freq[num] += 1
