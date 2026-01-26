522ccf55e8cfa1c17b277c1d032e2a59713c06e8
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 17:23:10 +0700

    Time: 50 ms (81.82%), Space: 55.6 MB (16.53%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,14 +1,11 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        res = energy = sum(t[0] for t in tasks)
-        heap = []
-        for task in tasks:
-            heapq.heappush(heap, (task[0] - task[1], task[0], task[1]))
-        while heap:
-            _, actual, minimum = heapq.heappop(heap)
+        tasks.sort(key=lambda x: x[1] - x[0], reverse=True)
+        energy = 0
+        res = 0
+        for actual, minimum in tasks:
             if energy < minimum:
                 res += minimum - energy
-                energy = minimum - actual
-            else:
-                energy -= actual
+                energy = minimum
+            energy -= actual
         return res
