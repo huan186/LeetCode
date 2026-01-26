50cd9beaed06d824811252e9e5c655a32326364b
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-26 16:05:08 +0700

    Time: 55 ms (88.89%), Space: 25.4 MB (14.81%) - LeetHub

--- 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
@@ -1,39 +1,25 @@
 class Solution:
     def maxEqualFreq(self, nums: List[int]) -> int:
-        from collections import Counter
-
         freq = Counter(nums)
-        cnt = Counter(freq.values())
 
         def valid():
-            if len(cnt) == 1:
-                f = next(iter(cnt))
-                return f == 1 or cnt[f] == 1
-
-            if len(cnt) == 2:
-                f1, f2 = min(cnt), max(cnt)
-                return (
-                    f1 == 1 and cnt[f1] == 1 or
-                    f2 == f1 + 1 and cnt[f2] == 1
-                )
-
-            return False
+            cnt = Counter(freq.values())
+            n = len(cnt)
+            min_key, max_key = min(cnt.keys()), max(cnt.keys())
+            return (
+                    n == 1 and (min_key == 1 or min(cnt.values()) == 1)
+                    or n == 2 and (
+                            1 in cnt and cnt[1] == 1
+                            or max_key - min_key == 1 and cnt[max_key] == 1
+                    )
+            )
 
         for i in range(len(nums) - 1, -1, -1):
             if valid():
                 return i + 1
-
-            x = nums[i]
-            f = freq[x]
-
-            cnt[f] -= 1
-            if cnt[f] == 0:
-                del cnt[f]
-
-            if f == 1:
-                del freq[x]
+            if freq[nums[i]] == 1:
+                freq.pop(nums[i])
             else:
-                freq[x] -= 1
-                cnt[f - 1] += 1
+                freq[nums[i]] -= 1
 
         return 1
