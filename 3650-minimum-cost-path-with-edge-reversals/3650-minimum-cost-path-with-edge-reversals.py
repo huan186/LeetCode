23423c3cbada859f8a516b75dadf700008847d97
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-27 09:44:42 +0700

    Time: 878 ms (24.82%), Space: 69.6 MB (81.15%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[] for _ in range(n)]
+        for u, v, cost in edges:
+            graph[u].append((v, cost))
+            graph[v].append((u, 2 * cost))
+        min_cost = [10**9] * n
+        heap = [(0, 0)]
+        while heap:
+            total_cost, node = heapq.heappop(heap)
+            if min_cost[node] <= total_cost:
+                continue
+            min_cost[node] = total_cost
+            for next_node, cost in graph[node]:
+                if total_cost + cost < min_cost[next_node]:
+                    heapq.heappush(heap, (total_cost + cost, next_node))
+        return min_cost[-1] if min_cost[-1] != 10**9 else -1
