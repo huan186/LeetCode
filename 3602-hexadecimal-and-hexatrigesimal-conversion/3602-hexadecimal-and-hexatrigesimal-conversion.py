113861ccb3027dbe946cfb82d6bf57c53993bcd9
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-28 23:34:18 +0700

    Time: 0 ms (100%), Space: 19.4 MB (29.5%) - LeetHub

--- 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.py
+++ 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.py
@@ -1,13 +1,10 @@
 class Solution:
     def concatHex36(self, n: int) -> str:
+        chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         def base_10_to_base_x(x, v10):
-            if v10 == 0:
-                return '0'
             vx = ''
             while v10 > 0:
-                r = v10 % x
-                ch = str(r) if r < 10 else chr(ord('A') + r - 10)
-                vx = ch + vx
+                vx = chars[v10 % x] + vx
                 v10 //= x
             return vx
         return base_10_to_base_x(16, n ** 2) + base_10_to_base_x(36, n ** 3)
