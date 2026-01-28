0604041b05cfc4cefedb019b51c8b35842158a4b
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-28 23:29:55 +0700

    Time: 3 ms (34.5%), Space: 19.3 MB (34.5%) - LeetHub

---
+++ 3602-hexadecimal-and-hexatrigesimal-conversion/3602-hexadecimal-and-hexatrigesimal-conversion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def concatHex36(self, n: int) -> str:
+        def base_10_to_base_x(x, v10):
+            if v10 == 0:
+                return '0'
+            vx = ''
+            while v10 > 0:
+                r = v10 % x
+                ch = str(r) if r < 10 else chr(ord('A') + r - 10)
+                vx = ch + vx
+                v10 //= x
+            return vx
+        return base_10_to_base_x(16, n * n) + base_10_to_base_x(36, n * n * n)
