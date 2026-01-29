942abd7e1c6ae4bedd24b4a2dd14f97c06c912d0
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-29 11:14:09 +0700

    Time: 327 ms (96.62%), Space: 21 MB (28.27%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -1,10 +1,14 @@
 class Solution:
     def minimumCost(self, source, target, original, changed, cost):
-        graph = [[] for _ in range(26)]
+        graph = [{} for _ in range(26)]
         for o, c, w in zip(original, changed, cost):
-            graph[ord(o)-97].append((ord(c)-97, w))
-        INF = 10**18
-        dist = [[INF]*26 for _ in range(26)]
+            i1, i2 = ord(o) - 97, ord(c) - 97
+            if i2 in graph[i1]:
+                graph[i1][i2] = min(graph[i1][i2], w)
+            else:
+                graph[i1][i2] = w
+        inf = 10 ** 18
+        dist = [[inf] * 26 for _ in range(26)]
         for s in range(26):
             heap = [(0, s)]
             while heap:
@@ -12,13 +16,13 @@ class Solution:
                 if dist[s][u] <= cur_cost:
                     continue
                 dist[s][u] = cur_cost
-                for v, w in graph[u]:
+                for v, w in graph[u].items():
                     if cur_cost + w < dist[s][v]:
                         heapq.heappush(heap, (cur_cost + w, v))
         ans = 0
         for a, b in zip(source, target):
-            u, v = ord(a)-97, ord(b)-97
-            if dist[u][v] == INF:
+            u, v = ord(a) - 97, ord(b) - 97
+            if dist[u][v] == inf:
                 return -1
             ans += dist[u][v]
         return ans
