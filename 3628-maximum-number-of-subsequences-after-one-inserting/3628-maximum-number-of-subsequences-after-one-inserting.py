644124ac5d8177ae4c327b5c17cce1ae49ff92af
huan186 <131932368+huan186@users.noreply.github.com>
2026-01-31 00:29:25 +0700

    Time: 137 ms (99.77%), Space: 19.9 MB (98.68%) - LeetHub

--- 3628-maximum-number-of-subsequences-after-one-inserting/3628-maximum-number-of-subsequences-after-one-inserting.py
+++ 3628-maximum-number-of-subsequences-after-one-inserting/3628-maximum-number-of-subsequences-after-one-inserting.py
@@ -1,27 +1,28 @@
 class Solution:
     def numOfSubsequences(self, s: str) -> int:
-        n = len(s)
-        pref = [[0] * 3 for _ in range(n + 1)]
-        for i in range(n):
-            pref[i + 1][0] = pref[i][0] + (s[i] == 'L')
-            pref[i + 1][1] = pref[i][1] + (s[i] == 'C')
-            pref[i + 1][2] = pref[i][2] + (s[i] == 'T')
+        total_T = s.count('T')
 
+        L_cnt = 0
+        LC_cnt = 0
         base_LCT = 0
+
+        gain_insert_C = 0
         gain_insert_L = 0
         gain_insert_T = 0
-        gain_insert_C = 0
 
-        for i in range(n):
-            if s[i] == 'C':
-                L_before = pref[i][0]
-                T_after = pref[n][2] - pref[i + 1][2]
-                base_LCT += L_before * T_after
-                gain_insert_L += T_after
-                gain_insert_T += L_before
-            gain_insert_C = max(
-                gain_insert_C,
-                pref[i + 1][0] * (pref[n][2] - pref[i + 1][2])
-            )
+        T_remaining = total_T
+
+        for ch in s:
+            if ch == 'L':
+                L_cnt += 1
+            elif ch == 'C':
+                base_LCT += L_cnt * T_remaining
+                LC_cnt += L_cnt
+                gain_insert_L += T_remaining
+                gain_insert_T += L_cnt
+            elif ch == 'T':
+                T_remaining -= 1
+
+            gain_insert_C = max(gain_insert_C, L_cnt * T_remaining)
 
         return base_LCT + max(gain_insert_L, gain_insert_C, gain_insert_T)
