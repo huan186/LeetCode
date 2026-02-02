98e9256dd6971ef19672cee44f7a0c64a6fd587d
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-02 17:22:01 +0700

    Time: 27 ms (97.02%), Space: 19.5 MB (64.68%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
@@ -1,27 +1,24 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        from collections import deque
+        n = len(s)
+        res = None
+        t = list(map(int, s)) * 2
 
-        seen = set()
-        q = deque([s])
-        res = s
+        for shift in range(0, lcm(n, b), b):
+            idx = n - (shift % n)
+            e = 0
+            min_start = 10 * t[idx] + t[idx + 1]
+            for c in range(1, 100 if b % 2 else 10):
+                c1, c2 = c // 10, c % 10
+                d1, d2 = (t[idx] + c1 * a) % 10, (t[idx + 1] + c2 * a) % 10
+                if 10 * d1 + d2 < min_start:
+                    min_start = 10 * d1 + d2
+                    e = c
+            c1, c2 = e // 10, e % 10
+            temp = t[idx: idx + n]
+            for i in range(n):
+                temp[i] = (temp[i] + (c2 if i % 2 else c1) * a) % 10
+            if not res or temp < res:
+                res = temp
 
-        while q:
-            cur = q.popleft()
-            if cur in seen:
-                continue
-            seen.add(cur)
-
-            res = min(res, cur)
-
-            lst = list(cur)
-            for i in range(1, len(lst), 2):
-                lst[i] = str((int(lst[i]) + a) % 10)
-            add_str = ''.join(lst)
-
-            rot_str = cur[-b:] + cur[:-b]
-
-            q.append(add_str)
-            q.append(rot_str)
-
-        return res
+        return ''.join(map(str, res))
