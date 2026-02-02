cd641df5a47993254479f1a31b538f345c001aec
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-02 17:22:59 +0700

    Time: 20 ms (98.01%), Space: 19.5 MB (59.2%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.py
@@ -1,24 +1,26 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
         n = len(s)
-        res = None
-        t = list(map(int, s)) * 2
+        best = None
+        digits = list(map(int, s)) * 2
 
         for shift in range(0, lcm(n, b), b):
-            idx = n - (shift % n)
-            e = 0
-            min_start = 10 * t[idx] + t[idx + 1]
-            for c in range(1, 100 if b % 2 else 10):
-                c1, c2 = c // 10, c % 10
-                d1, d2 = (t[idx] + c1 * a) % 10, (t[idx + 1] + c2 * a) % 10
-                if 10 * d1 + d2 < min_start:
-                    min_start = 10 * d1 + d2
-                    e = c
-            c1, c2 = e // 10, e % 10
-            temp = t[idx: idx + n]
-            for i in range(n):
-                temp[i] = (temp[i] + (c2 if i % 2 else c1) * a) % 10
-            if not res or temp < res:
-                res = temp
+            start_idx = n - (shift % n)
 
-        return ''.join(map(str, res))
+            best_c0, best_c1 = 0, 0
+            d0, d1 = digits[start_idx], digits[start_idx + 1]
+            min_pref = 10 * d0 + d1
+            for c0 in range(10 if b % 2 else 1):
+                for c1 in range(10):
+                    pref = 10 * ((d0 + c0 * a) % 10) + (d1 + c1 * a) % 10
+                    if pref < min_pref:
+                        min_pref = pref
+                        best_c0, best_c1 = c0, c1
+
+            t = digits[start_idx: start_idx + n]
+            for i in range(n):
+                t[i] = (t[i] + (best_c1 if i % 2 else best_c0) * a) % 10
+            if not best or t < best:
+                best = t
+
+        return ''.join(map(str, best))
