b84fd6bf53ce16d7f603b87b18d60204747aabd3
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-03 15:28:03 +0700

    Time: 4 ms (63.59%), Space: 19.4 MB (35.16%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -6,8 +6,8 @@ class Solution:
         for word in words:
             curr = 0
             for ch in word:
-                curr += 11 ** (ord(ch) - 97)
-            if curr != prev:
+                curr += 2.21 ** (ord(ch) - 97)
+            if abs(curr - prev) > 1e-5:
                 prev = curr
                 ans.append(word)
 
