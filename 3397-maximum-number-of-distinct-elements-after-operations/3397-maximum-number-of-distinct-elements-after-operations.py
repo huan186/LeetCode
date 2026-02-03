7658acd34a87ee9aacd3264584173ec6de024abd
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-03 14:23:26 +0700

    Time: 629 ms (95.36%), Space: 33.2 MB (35.52%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.py
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
-        n = len(nums)
         nums.sort()
         cnt = 0
         prev = - 10 ** 18
-        for i in range(n):
-            diff = max((prev + 1 - nums[i]), -k)
-            if diff > k:
+        for num in nums:
+            if num + k == prev:
                 continue
+            prev = max(prev + 1, num - k)
             cnt += 1
-            prev = nums[i] + diff
         return cnt
             
