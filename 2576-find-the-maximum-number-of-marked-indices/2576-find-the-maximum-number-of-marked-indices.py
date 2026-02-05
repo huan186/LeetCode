082318ac37d7a9c7f025b3769e9fa6bd8c867473
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-06 01:14:42 +0700

    Time: 71 ms (87.5%), Space: 32.8 MB (13.54%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
@@ -3,9 +3,7 @@ class Solution:
         nums.sort()
         n = len(nums)
         i = 0
-        ans = 0
         for j in range((n + 1) // 2, n):
             if (nums[i] << 1) <= nums[j]:
-                ans += 2
                 i += 1
-        return ans
+        return i << 1
