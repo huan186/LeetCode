57dc59bc821da4eba1d83141bf767a319d72b370
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-05 14:34:08 +0700

    Time: 51 ms (31.58%), Space: 19.6 MB (28.93%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        
+        if n <= 2:
+            return n
+
+        def lcm(a, b):
+            return a if b == 0 else lcm(b, a % b)
+
+        res = 0
+
+        def cal_slope(i1, i2):
+            x1, y1 = points[i1]
+            x2, y2 = points[i2]
+            dx = x1 - x2
+            dy = y1 - y2
+            if dx == 0:
+                return 'inf'
+            l = lcm(dx, dy)
+            dx //= l
+            dy //= l
+            if dx < 0:
+                dx = -dx
+                dy = -dy
+            return '{}/{}'.format(dx, dy)
+
+        for i in range(n - 1):
+            slopes = {}
+            for j in range(i + 1, n):
+                slope = cal_slope(i, j)
+                slopes[slope] = slopes.get(slope, 1) + 1
+                res = max(res, slopes[slope])
+
+        return res
