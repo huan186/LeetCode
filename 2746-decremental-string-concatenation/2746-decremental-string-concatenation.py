6db73472688b65cb922ba0d9de26a9e1fe332f9d
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-05 20:44:10 +0700

    Time: 175 ms (88.89%), Space: 19.8 MB (54.63%) - LeetHub

--- 2746-decremental-string-concatenation/2746-decremental-string-concatenation.py
+++ 2746-decremental-string-concatenation/2746-decremental-string-concatenation.py
@@ -1,14 +1,13 @@
 class Solution:
     def minimizeConcatenatedLength(self, words: List[str]) -> int:
+        inf = float('inf')
         pairs = {}
         w0 = words[0]
         pairs[(w0[0], w0[-1])] = len(w0)
         for w in words[1:]:
-            l = len(w)
-            s1, e1 = w[0], w[-1]
+            s1, e1, l = w[0], w[-1], len(w)
             nxt = {}
-            for p, v in pairs.items():
-                s2, e2 = p
+            for (s2, e2), v in pairs.items():
                 nxt[(s1, e2)] = min(nxt.get((s1, e2), inf), v + l - (1 if e1 == s2 else 0))
                 nxt[(s2, e1)] = min(nxt.get((s2, e1), inf), v + l - (1 if s1 == e2 else 0))
             pairs = nxt
