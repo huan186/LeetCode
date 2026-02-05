39718cc1918441f8db72e59ac94af4a4efbbac00
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-06 01:05:11 +0700

    Time: 133 ms (89.26%), Space: 20.8 MB (30.53%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.py
+++ 0477-total-hamming-distance/0477-total-hamming-distance.py
@@ -1,7 +1,8 @@
 class Solution:
     def totalHammingDistance(self, nums: List[int]) -> int:
         ans, n = 0, len(nums)
-        for i in range(30):
+        max_i = max(nums).bit_length()
+        for i in range(max_i):
             bit_cnt = 0
             for num in nums:
                 bit_cnt += (num >> i) & 1
