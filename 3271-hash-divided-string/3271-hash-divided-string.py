0c66b68cd18f61f1a0051a3c4be57a28f3b8dd72
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-06 14:54:55 +0700

    Time: 19 ms (45.03%), Space: 19.5 MB (5.5%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.py
+++ 3271-hash-divided-string/3271-hash-divided-string.py
@@ -2,8 +2,6 @@ class Solution:
     def stringHash(self, s: str, k: int) -> str:
         res = []
         for i in range(0, len(s), k):
-            hv = 0
-            for j in range(k):
-                hv += ord(s[i + j]) - ord('a')
+            hv = sum(ord(s[i + j]) for j in range(k)) - k * 97
             res.append(chr(hv % 26 + ord('a')))
         return ''.join(res)
