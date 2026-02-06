bbabfacc043da030ab64519ba53efcfa0e816a39
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-06 14:24:37 +0700

    Time: 18 ms (85.16%), Space: 21 MB (42.58%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,28 +1,31 @@
 class Solution:
-    def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-
+    def closestCost(self, baseCosts, toppingCosts, target):
         topping_costs = {0}
+        n = len(toppingCosts)
         def dfs(i, c):
-            if i == len(toppingCosts):
+            if i == n:
                 return
+
             dfs(i + 1, c)
+
             c1 = c + toppingCosts[i]
-            dfs(i + 1, c1)
             topping_costs.add(c1)
-            if c1 == 15:
-                print(topping_costs)
-            if c1 <= target:
+            dfs(i + 1, c1)
+
+            if c1 < target:
                 c2 = c1 + toppingCosts[i]
                 topping_costs.add(c2)
-                if c2 == 15:
-                    print(topping_costs)
                 dfs(i + 1, c2)
+
         dfs(0, 0)
-        min_diff = 10 ** 18
-        for bc in baseCosts:
-            for tc in topping_costs:
-                cost = bc + tc
-                diff = cost - target
-                if abs(diff) < abs(min_diff) or abs(diff) == abs(min_diff) and diff < min_diff:
-                    min_diff = diff
-        return target + min_diff
+
+        best = float('inf')
+        for b in baseCosts:
+            for t in topping_costs:
+                total = b + t
+                diff = total - target
+                d = abs(diff) - abs(best)
+                if d < 0 or d == 0 and diff < best:
+                    best = diff
+
+        return target + best
