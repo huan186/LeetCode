341e80ff6bd333780bc30b6ae75f7c41cf9e9310
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-06 14:55:19 +0700

    Time: 15 ms (74.08%), Space: 19.3 MB (45.29%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.py
+++ 3271-hash-divided-string/3271-hash-divided-string.py
@@ -3,5 +3,5 @@ class Solution:
         res = []
         for i in range(0, len(s), k):
             hv = sum(ord(s[i + j]) for j in range(k)) - k * 97
-            res.append(chr(hv % 26 + ord('a')))
+            res.append(chr(hv % 26 + 97))
         return ''.join(res)
