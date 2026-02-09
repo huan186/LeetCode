1249b2e82b0d477ce20ed7e007ebecf1e7a5107c
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-09 09:08:14 +0700

    Time: 27 ms (82.76%), Space: 24.7 MB (37.15%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -1,19 +1,22 @@
 class Solution:
     def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        nums = []
+        nodes = []
 
         def inorder(node):
             if not node:
                 return
             inorder(node.left)
-            nums.append(node.val)
+            nodes.append(node)
             inorder(node.right)
 
         def build(l, r):
             if l > r:
                 return None
-            m = l + (r - l) // 2
-            return TreeNode(nums[m], build(l, m - 1), build(m + 1, r))
+            m = (l + r) // 2
+            root = nodes[m]
+            root.left = build(l, m - 1)
+            root.right = build(m + 1, r)
+            return root
 
         inorder(root)
-        return build(0, len(nums) - 1)
+        return build(0, len(nodes) - 1)
