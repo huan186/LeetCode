5755f42967c47908837f838e618749a22b7f0481
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-12 15:13:43 +0700

    Time: 19 ms (78.53%), Space: 26.6 MB (23.16%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,37 +1,29 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        # Step 1: Calculate in-degree
         in_deg = [0] * n
-        out_deg = [0] * n
-
-        def helper(node, next_node):
-            if next_node != -1:
-                out_deg[node] += 1
-                in_deg[next_node] += 1
 
         for i in range(n):
-            helper(i, leftChild[i])
-            helper(i, rightChild[i])
+            for child in (leftChild[i], rightChild[i]):
+                if child != -1:
+                    if in_deg[child] == 1:
+                        return False
+                    in_deg[child] += 1
 
-        if sum(in_deg) != n - 1:
+        # Step 2: Find root
+        roots = [i for i in range(n) if in_deg[i] == 0]
+        if len(roots) != 1:
             return False
 
-        root = -1
-        for i in range(n):
-            if in_deg[i] == 0:
-                if root != -1:
-                    return False
-                root = i
-            if in_deg[i] > 1 or out_deg[i] > 2:
-                return False
+        root = roots[0]
 
+        # Step 3: DFS
         visited = set()
-
         def dfs(node):
+            if node in visited:
+                return False
             visited.add(node)
-            if leftChild[node] != -1 and leftChild[node] not in visited:
-                dfs(leftChild[node])
-            if rightChild[node] != -1 and rightChild[node] not in visited:
-                dfs(rightChild[node])
+            return ((leftChild[node] == -1 or dfs(leftChild[node]))
+                and (rightChild[node] == -1 or dfs(rightChild[node])))
 
-        dfs(root)
-        return len(visited) == n
+        return dfs(root) and len(visited) == n
