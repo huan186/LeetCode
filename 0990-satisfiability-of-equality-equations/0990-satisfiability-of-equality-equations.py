fb6dafb174485a6b1c466511935cd9c0ae258d5b
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-12 14:12:23 +0700

    Time: 0 ms (100%), Space: 19.5 MB (29.05%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,30 +1,21 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        graph = defaultdict(set)
-        for x, op, _, y in equations:
-            ix = ord(x) - ord('a')
-            iy = ord(y) - ord('a')
-            if op == '=':
-                graph[ix].add(iy)
-                graph[iy].add(ix)
-        visited = [-1] * 26
-        def dfs(curr, v):
-            visited[curr] = v
-            for _next in graph[curr]:
-                if visited[_next] == -1:
-                    dfs(_next, v)
+        root = list(range(26))
 
-        cnt = 0
-        for i in range(26):
-            if visited[i] == -1:
-                dfs(i, cnt)
-                cnt += 1
+        def find(x):
+            if root[x] != x:
+                root[x] = find(root[x])
+            return root[x]
+
+        def union(x, y):
+            root[find(x)] = find(y)
+
+        for e in equations:
+            if e[1] == '=':
+                union(ord(e[0]) - 97, ord(e[3]) - 97)
                 
-        for x, op, _, y in equations:
-            ix = ord(x) - ord('a')
-            iy = ord(y) - ord('a')
-            if op == '!':
-                if visited[ix] == visited[iy]:
-                    return False
+        for e in equations:
+            if e[1] == '!' and find(ord(e[0]) - 97) == find(ord(e[3]) - 97):
+                return False
 
         return True
