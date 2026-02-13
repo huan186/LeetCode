aac6a526f08bb0505142fa3e6358d61eca5e7dd5
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-13 11:20:43 +0700

    Time: 1962 ms (40.88%), Space: 123.8 MB (13.14%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
@@ -2,20 +2,18 @@ class Solution:
     def longestBalanced(self, s: str) -> int:
         n = len(s)
         cnt = [0] * 3
-        states = {}
-        for i in range(7):
-            states[(i, 0, 0)] = -1
+        states = {(i, 0, 0): -1 for i in range(7)}
         res = 0
         for i in range(n):
             cnt[ord(s[i])-ord('a')] += 1
             for x in [
-                (1, cnt[0], cnt[1]),
-                (2, cnt[0], cnt[2]),
-                (3, cnt[1], cnt[2]),
-                (4, cnt[0] - cnt[1], cnt[2]),
-                (5, cnt[0] - cnt[2], cnt[1]),
-                (6, cnt[1] - cnt[2], cnt[0]),
-                (7, cnt[0] - cnt[1], cnt[0] - cnt[2]),
+                (0, cnt[0], cnt[1]),
+                (1, cnt[0], cnt[2]),
+                (2, cnt[1], cnt[2]),
+                (3, cnt[0] - cnt[1], cnt[2]),
+                (4, cnt[0] - cnt[2], cnt[1]),
+                (5, cnt[1] - cnt[2], cnt[0]),
+                (6, cnt[0] - cnt[1], cnt[0] - cnt[2]),
             ]:
                 if x not in states:
                     states[x] = i
