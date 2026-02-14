976b3f48f9681b6204bc9b47772bc911cda745fa
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-14 13:08:37 +0700

    Time: 87 ms (47.49%), Space: 19.5 MB (50.36%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,10 +1,13 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         dp = [poured]
+        
         for row in range(1, query_row + 1):
             nxt = [0] * (row + 1)
-            for i in range(row):
-                nxt[i] += max((dp[i] - 1) / 2, 0)
-                nxt[i + 1] += max((dp[i] - 1) / 2, 0)
+            for i in range(len(dp)):
+                overflow = max(dp[i] - 1, 0) / 2
+                nxt[i] += overflow
+                nxt[i + 1] += overflow
             dp = nxt
+        
         return min(dp[query_glass], 1)
