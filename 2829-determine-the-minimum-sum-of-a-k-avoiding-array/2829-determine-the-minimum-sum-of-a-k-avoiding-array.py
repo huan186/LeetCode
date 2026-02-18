0dc352354f998fc2506db029d15fa8a05d660d72
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-19 02:30:13 +0700

    Time: 0 ms (100%), Space: 19.4 MB (22.73%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.py
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def minimumSum(self, n: int, k: int) -> int:
         def consecutive_sum(x):
-            return x * (x + 1) >> 1
-        half = k >> 1
+            return x * (x + 1) // 2
+        half = k // 2
         if n <= half:
             return consecutive_sum(n)
         r = n - half
-        return consecutive_sum(half) + consecutive_sum(r) + r * (k - 1)
+        return consecutive_sum(half) + r * k + consecutive_sum(r - 1)
