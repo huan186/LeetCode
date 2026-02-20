a4328db61dd6501ab20610943db145ce1e407d50
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-20 19:34:38 +0700

    Time: 0 ms (100%), Space: 19.2 MB (65.79%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.py
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -1,16 +1,15 @@
 class Solution:
-    @lru_cache(None)
     def makeLargestSpecial(self, s: str) -> str:
-        i, n = 0, len(s)
-        parts = []
-        while i < n:
-            d = 1
-            j = i + 1
-            while j < n and d > 0:
-                d += 1 if s[j] == '1' else -1
-                j += 1
-            parts.append('1' + self.makeLargestSpecial(s[i + 1 : j - 1]) + '0')
-            i = j
-        parts.sort(reverse=True)
-        return ''.join(parts)
-                
+        stack = [[]]
+        
+        for ch in s:
+            if ch == '1':
+                stack.append([])
+            else:
+                cur = stack.pop()
+                cur.sort(reverse=True)
+                block = '1' + ''.join(cur) + '0'
+                stack[-1].append(block)
+        
+        stack[0].sort(reverse=True)
+        return ''.join(stack[0])
