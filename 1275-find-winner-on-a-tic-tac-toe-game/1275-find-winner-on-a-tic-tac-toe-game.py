1afdd99a071b56bba0aa78750aeaf4de4f4d597a
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-23 01:29:42 +0700

    Time: 0 ms (100%), Space: 19.5 MB (27.46%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,20 +1,17 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        board = [[None] * 3 for _ in range(3)]
-        p = 'A'
-        for x, y in moves:
-            board[x][y] = p
-            p = 'A' if p == 'B' else 'B'
-        def helper(x, y, dx, dy):
-            return board[x][y] and board[x][y] == board[x + dx][y + dy] == board[x + 2 * dx][y + 2 * dy]
-        if helper(0, 0, 0, 1) or helper(0, 0, 1, 1) or helper(0, 0, 1, 0):
-            return board[0][0]
-        if helper(0, 1, 1, 0):
-            return board[0][1]
-        if helper(0, 2, 1, -1) or helper(0, 2, 1, 0):
-            return board[0][2]
-        if helper(1, 0, 0, 1):
-            return board[1][0]
-        if helper(2, 0, 0, 1):
-            return board[2][0]
-        return 'Draw' if len(moves) == 9 else 'Pending'
+        rows = [0] * 3
+        cols = [0] * 3
+        diag = anti = 0
+        player = 1
+        for r, c in moves:
+            rows[r] += player
+            cols[c] += player
+            if r == c:
+                diag += player
+            if r + c == 2:
+                anti += player
+            if abs(rows[r]) == 3 or abs(cols[c]) == 3 or abs(diag) == 3 or abs(anti) == 3:
+                return "A" if player == 1 else "B"
+            player *= -1 
+        return "Draw" if len(moves) == 9 else "Pending"
