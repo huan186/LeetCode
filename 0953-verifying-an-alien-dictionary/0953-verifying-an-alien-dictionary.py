d3b636ab6b78d70e7d9c901b808c01e4a56f3d6d
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-22 22:39:01 +0700

    Time: 0 ms (100%), Space: 19.5 MB (28.9%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,11 +1,16 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        mapping = {order[i] : chr(i + 97) for i in range(len(order))}
-        def convert(s):
-            return ''.join(mapping[c] for c in s)
-        for i in range(len(words)):
-            words[i] = convert(words[i])
+        mapping = {order[i] : i for i in range(len(order))}
+        def is_smaller(s1, s2):
+            l1, l2 = len(s1), len(s2)
+            for i in range(min(l1, l2)):
+                d = mapping[s2[i]] - mapping[s1[i]]
+                if d > 0:
+                    return True
+                if d < 0:
+                    return False
+            return l1 <= l2
         for i in range(len(words) - 1):
-            if words[i] > words[i + 1]:
+            if not is_smaller(words[i], words[i + 1]):
                 return False
         return True
