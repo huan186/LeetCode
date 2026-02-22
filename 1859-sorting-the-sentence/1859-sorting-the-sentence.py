7c5c08dab8836a20f98785056c209987759aa0ce
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-23 00:51:17 +0700

    Time: 41 ms (66.95%), Space: 19.4 MB (43.22%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        splits = s.split(' ')
+        words = [None] * len(splits)
+        for w in splits:
+            idx = int(w[-1]) - 1
+            words[idx] = w[:-1]
+        return ' '.join(words)
