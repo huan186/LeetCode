82e2f094d0c28d468ddfa7764401205809eceb47
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-23 16:33:11 +0700

    Time: 0 ms (100%), Space: 19.3 MB (66.6%) - LeetHub

--- 0593-valid-square/0593-valid-square.py
+++ 0593-valid-square/0593-valid-square.py
@@ -4,11 +4,13 @@ class Solution:
             return (pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2
         
         p = [p1, p2, p3, p4]
-        sd = []
-        for i in range(3):
-            for j in range(i + 1, 4):
-                sd.append(square_distance(p[i], p[j]))
 
+        sd = [square_distance(p[i], p[j]) for i in range(3) for j in range(i + 1, 4)]
         sd.sort()
 
-        return sd[0] != 0 and sd[0] == sd[1] == sd[2] == sd[3] and sd[4] == sd[5] and sd[4] == 2 * sd[0]
+        return (
+            sd[0] != 0
+            and sd[0] == sd[1] == sd[2] == sd[3]
+            and sd[4] == sd[5]
+            and sd[4] == 2 * sd[0]
+        )
