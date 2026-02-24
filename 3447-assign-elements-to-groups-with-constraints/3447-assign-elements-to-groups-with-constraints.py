ca0a33d3be1564bf20b688d7adee7064e5f51292
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-24 11:05:59 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3447-assign-elements-to-groups-with-constraints/3447-assign-elements-to-groups-with-constraints.py
@@ -0,0 +1,11 @@
+class Solution:
+    def assignElements(self, groups: List[int], elements: List[int]) -> List[int]:
+        seen = set()
+        indices = [-1] * 100001
+        for i, v in enumerate(elements):
+            if v in seen:
+                continue
+            for j in range(v, 100001, v):
+                if indices[j] == -1:
+                    indices[j] = i
+        return [indices[g] for g in groups]
