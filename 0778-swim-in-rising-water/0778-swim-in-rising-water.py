34272e0207d36775ace7eab6820770d1c610c8df
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-24 11:44:44 +0700

    Time: 44 ms (22.16%), Space: 20.2 MB (13.38%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,36 +1,23 @@
 class Solution:
-    def swimInWater(self, grid: List[List[int]]) -> int:
+    def swimInWater(self, grid):
         n = len(grid)
-
-
+        heap = [(grid[0][0], 0, 0)]
         visited = set()
-
-        dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
-        def dfs(x, y, k):
-            if grid[x][y] > k:
-                return False
+        
+        while heap:
+            t, x, y = heapq.heappop(heap)
+            
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+            
             if x == n - 1 and y == n - 1:
-                return True
-            for dx, dy in dirs:
+                return t
+            
+            for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in visited:
-                    visited.add((nx, ny))
-                    if dfs(nx, ny, k):
-                        return True
-            return False
-
-
-        ans = 0
-        left, right = 0, n * n - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-            visited.clear()
-            if dfs(0, 0, mid):
-                right = mid - 1
-                ans = mid
-            else:
-                left = mid + 1
-
-        return ans
+                if 0 <= nx < n and 0 <= ny < n:
+                    heapq.heappush(
+                        heap,
+                        (max(t, grid[nx][ny]), nx, ny)
+                    )
