a047bc0f54fc75e56eaa78ee960f5b3c7004e250
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-24 11:46:29 +0700

    Time: 45 ms (21.77%), Space: 20.1 MB (23.27%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -3,21 +3,17 @@ class Solution:
         n = len(grid)
         heap = [(grid[0][0], 0, 0)]
         visited = set()
-        
+
         while heap:
             t, x, y = heapq.heappop(heap)
-            
             if (x, y) in visited:
                 continue
             visited.add((x, y))
-            
             if x == n - 1 and y == n - 1:
                 return t
-            
-            for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < n and 0 <= ny < n:
-                    heapq.heappush(
-                        heap,
-                        (max(t, grid[nx][ny]), nx, ny)
-                    )
+                    heapq.heappush(heap, (max(t, grid[nx][ny]), nx, ny))
+
+        return None
