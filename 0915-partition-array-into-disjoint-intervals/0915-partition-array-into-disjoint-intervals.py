7544a3565317693bd98dfb0f4936b0d2b29b2001
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-25 13:39:43 +0700

    Time: 81 ms (43.33%), Space: 31.9 MB (36.3%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.py
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.py
@@ -5,7 +5,7 @@ class Solution:
         min_postfix[-1] = nums[-1]
         for i in range(n - 2, -1, -1):
             min_postfix[i] = min(nums[i + 1], min_postfix[i + 1])
-        largest_left = 0
+        largest_left = nums[0]
         for i in range(n):
             largest_left = max(largest_left, nums[i])
             if largest_left <= min_postfix[i]:
