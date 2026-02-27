d02e02800761fb57a6d3462c44a37bf960d0dab9
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-27 14:37:54 +0700

    Time: 28 ms (77.34%), Space: 20.2 MB (33.53%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -2,4 +2,4 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         if len(s) < k:
             return False
-        return sum(1 for v in Counter(s).values() if v % 2) <= k
+        return sum(v & 1 for v in Counter(s).values()) <= k
