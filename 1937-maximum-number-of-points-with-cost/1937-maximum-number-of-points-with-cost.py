db872ea8dbdc4c93593b76481f72c244e1edee7b
huan186 <131932368+huan186@users.noreply.github.com>
2026-02-27 14:05:42 +0700

    Time: 223 ms (94.29%), Space: 42.4 MB (51.05%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -3,14 +3,11 @@ class Solution:
         n = len(points[0])
         dp = [0] * n
         for row in points:
-            x = 0
-            for i in range(n):
-                dp[i] = max(dp[i], x)
-                x = dp[i] - 1
-            x = 0
-            for i in range(n - 1, -1, -1):
-                dp[i] = max(dp[i], x)
-                x = dp[i] - 1
+            for start, end, step in ((0, n, 1), (n - 1, -1, -1)):
+                x = 0
+                for i in range(start, end, step):
+                    dp[i] = max(dp[i], x)
+                    x = dp[i] - 1
             for i in range(n):
                 dp[i] += row[i]
         return max(dp)
