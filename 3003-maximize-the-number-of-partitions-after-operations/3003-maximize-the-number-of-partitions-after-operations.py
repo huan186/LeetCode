28e332c9b155fc64b1226dbad9e6d49d41d0c8b8
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-02 11:13:58 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.py
+++ 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.py
@@ -1,47 +1,32 @@
 class Solution:
     def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
         n = len(s)
-        left = [[0] * 3 for _ in range(n)]
-        right = [[0] * 3 for _ in range(n)]
 
-        num, mask, count = 0, 0, 0
-        for i in range(n - 1):
-            binary = 1 << (ord(s[i]) - ord("a"))
-            if not (mask & binary):
-                count += 1
-                if count <= k:
-                    mask |= binary
+        def count_partitions(change_index=-1, new_char=None):
+            partitions = 0
+            distinct = set()
+
+            for i in range(n):
+                c = s[i]
+                if i == change_index:
+                    c = new_char
+
+                if c not in distinct:
+                    if len(distinct) == k:
+                        partitions += 1
+                        distinct = set()
+                    distinct.add(c)
                 else:
-                    num += 1
-                    mask = binary
-                    count = 1
-            left[i + 1][0] = num
-            left[i + 1][1] = mask
-            left[i + 1][2] = count
+                    distinct.add(c)
 
-        num, mask, count = 0, 0, 0
-        for i in range(n - 1, 0, -1):
-            binary = 1 << (ord(s[i]) - ord("a"))
-            if not (mask & binary):
-                count += 1
-                if count <= k:
-                    mask |= binary
-                else:
-                    num += 1
-                    mask = binary
-                    count = 1
-            right[i - 1][0] = num
-            right[i - 1][1] = mask
-            right[i - 1][2] = count
+            return partitions + 1  # last segment
 
-        max_val = 0
+        # case no change
+        ans = count_partitions()
+
         for i in range(n):
-            seg = left[i][0] + right[i][0] + 2
-            tot_mask = left[i][1] | right[i][1]
-            tot_count = bin(tot_mask).count("1")
-            if left[i][2] == k and right[i][2] == k and tot_count < 26:
-                seg += 1
-            elif min(tot_count + 1, 26) <= k:
-                seg -= 1
-            max_val = max(max_val, seg)
-        return max_val
+            for c in map(chr, range(ord('a'), ord('z')+1)):
+                if c != s[i]:
+                    ans = max(ans, count_partitions(i, c))
+
+        return ans
