465fb1b4ce20ce698b7169c70c02de185be311be
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-02 15:02:13 +0700

    Time: 159 ms (95.38%), Space: 23.8 MB (19.08%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -9,12 +9,12 @@ class Solution:
             return min(flips, n - flips)
         dp = [0] * (n + 1)
         for i in range(n - 1, -1, -1):
-            dp[i] = dp[i + 1] + (0 if i % 2 == int(s[i]) else 1)
+            dp[i] = dp[i + 1] + (1 if i % 2 == int(s[i]) else 0)
         ans = min(dp[0], n - dp[0])
         flips = 0
         for i, c in enumerate(s):
             if i % 2 != int(c):
                 flips += 1
-            f = flips + n - i - 1 - dp[i + 1]
+            f = flips + dp[i + 1]
             ans = min(ans, f, n - f)
         return ans
