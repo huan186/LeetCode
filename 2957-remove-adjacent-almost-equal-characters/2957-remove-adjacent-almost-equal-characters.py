be84f353200519802f7b9cda6d351ab1ecad4a38
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-05 12:51:58 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.py
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.py
@@ -1,12 +1,3 @@
 class Solution:
     def removeAlmostEqualCharacters(self, word: str) -> int:
-        prev_ord = 0
-        res = 0
-        for c in word:
-            diff = abs(ord(c) - prev_ord)
-            if diff <= 1:
-                res += 1
-                prev_ord = 0
-            else:
-                prev_ord = ord(c)
-        return res
+        return (sum(abs(ord(a) - ord(b)) <= 1 for a, b in zip(word[:-1], word[1:])) + 1) // 2
