79f488ff709438ed413a9e935e8e72d8357cbed9
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-05 20:04:59 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
@@ -2,10 +2,10 @@ class Solution:
     def countWays(self, ranges: List[List[int]]) -> int:
         mod = 10 ** 9 + 7
         ranges.sort()
-        res = 1
+        cnt = 0
         prev_end = -1
         for start, end in ranges:
             if prev_end < start:
-                res = (res << 1) % mod
+                cnt += 1
             prev_end = max(prev_end, end)
-        return res
+        return (2 ** cnt) % mod
