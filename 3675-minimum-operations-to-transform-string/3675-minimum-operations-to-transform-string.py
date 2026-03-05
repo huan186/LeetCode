5ae76e0cec326e67a0d0bd80bc1f33ca5a547711
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-05 19:36:38 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.py
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.py
@@ -1,3 +1,6 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        return max((ord('a') - ord(c)) % 26 for c in s)
+        for i in range(1, 26):
+            if chr(ord('a') + i) in s:
+                return 26 - i
+        return 0
