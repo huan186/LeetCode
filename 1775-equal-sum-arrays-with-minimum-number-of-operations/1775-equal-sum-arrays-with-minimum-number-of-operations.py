1cfa5a9e00112eb092e97acbb1109505e8996577
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-05 14:38:29 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.py
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.py
@@ -1,26 +1,17 @@
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
-        m, n = len(nums1), len(nums2)
-        min_sum = max(m, n)
-        max_sum = min(6 * m, 6 * n)
-        if min_sum > max_sum:
-            return -1
         diff = sum(nums1) - sum(nums2)
-        if diff == 0:
-            return 0
         if diff < 0:
             nums1, nums2 = nums2, nums1
             diff = -diff
-        freq1 = Counter(nums1)
-        freq2 = Counter(nums2)
-        ans = 0
-        
-        for i in range(1, 6):
-            q = min(freq2[i], (diff + 5 - i) // (6 - i))
-            ans += q
-            diff -= min(diff, q * (6 - i))
-            j = 6 - i
-            q = min(freq1[j + 1], (diff + j - 1) // j)
-            ans += q
-            diff -= min(diff, q * j)
-        return ans if diff == 0 else -1
+        cnt = [0] * 6
+        for num in nums1:
+            cnt[num - 1] += 1
+        for num in nums2:
+            cnt[6 - num] += 1
+        res = 0
+        for gain in range(5, 0, -1):
+            q = min(cnt[gain], (diff + gain - 1) // gain)
+            res += q
+            diff -= min(diff, q * gain)
+        return res if diff == 0 else -1
