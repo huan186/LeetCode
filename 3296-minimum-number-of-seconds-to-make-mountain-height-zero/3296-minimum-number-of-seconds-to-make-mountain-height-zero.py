8941899721caaf67399c73a760b00a2024d5738c
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-13 13:20:07 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -1,6 +1,6 @@
 class Solution:
     def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
-        left, right = 1, 10 ** 18
+        left, right = 1, mountainHeight * (mountainHeight + 1) * min(workerTimes) // 2
         while left < right:
             mid = left + (right - left) // 2
 
@@ -8,9 +8,9 @@ class Solution:
 
             for wt in workerTimes:
                 height += math.floor((-1 + math.sqrt(1 + 8 * mid / wt)) / 2)
-
-            if height >= mountainHeight:
-                right = mid
+                if height >= mountainHeight:
+                    right = mid
+                    break   
             else:
                 left = mid + 1
 
