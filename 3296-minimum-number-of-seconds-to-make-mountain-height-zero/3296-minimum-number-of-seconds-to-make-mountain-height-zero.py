75832d43adb7953e2a377c41237bf71126daa292
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-13 13:51:35 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -2,9 +2,7 @@ class Solution:
     def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
         heap = [(w, 1, w) for w in workerTimes]
         heapq.heapify(heap)
-        res = 0
-        for _ in range(mountainHeight):
+        for _ in range(mountainHeight - 1):
             t, h, w = heapq.heappop(heap)
-            res = t
             heapq.heappush(heap, (t + (h + 1) * w, h + 1, w))
-        return res
+        return heap[0][0]
