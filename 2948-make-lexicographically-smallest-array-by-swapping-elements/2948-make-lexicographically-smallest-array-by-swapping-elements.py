970e1c0589d59ff9572c3092a221bdfad5c2799d
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-15 14:36:09 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,22 +1,17 @@
 class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        x = [(v, i) for i, v in enumerate(nums)]
-        x.sort()
-        n = len(x)
+    def lexicographicallySmallestArray(self, nums, limit):
+        n = len(nums)
+        x = sorted((v, i) for i, v in enumerate(nums))
         res = [0] * n
+
         i = 0
         while i < n:
-            indices = [x[i][1]]
-            values = [x[i][0]]
-            j = i + 1
-            while j < n and x[j][0] - x[j - 1][0] <= limit:
-                indices.append(x[j][1])
-                values.append(x[j][0])
+            j = i
+            while j + 1 < n and x[j + 1][0] - x[j][0] <= limit:
                 j += 1
-            indices.sort()
-            for idx, value in zip(indices, values):
-                res[idx] = value
-            i = j
-        return res
+            indices = sorted(idx for _, idx in x[i:j+1])
+            for k, idx in enumerate(indices):
+                res[idx] = x[i + k][0]
+            i = j + 1
 
-            
+        return res
