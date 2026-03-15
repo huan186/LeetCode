993cfda2e851edde0789d6f38dca64eb707cbd47
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-15 10:59:32 +0700

    Time: 251 ms (24.19%), Space: 56.2 MB (62.9%) - LeetHub

---
+++ 1622-fancy-sequence/1622-fancy-sequence.py
@@ -0,0 +1,25 @@
+class Fancy:
+
+    MOD = 10**9 + 7
+
+    def __init__(self):
+        self.arr = []
+        self.a = 1
+        self.b = 0
+
+    def append(self, val: int) -> None:
+        inv = pow(self.a, self.MOD - 2, self.MOD)
+        x = (val - self.b) * inv % self.MOD
+        self.arr.append(x)
+
+    def addAll(self, inc: int) -> None:
+        self.b = (self.b + inc) % self.MOD
+
+    def multAll(self, m: int) -> None:
+        self.a = self.a * m % self.MOD
+        self.b = self.b * m % self.MOD
+
+    def getIndex(self, idx: int) -> int:
+        if idx >= len(self.arr):
+            return -1
+        return (self.arr[idx] * self.a + self.b) % self.MOD
