f5fcc0da0349bfc0135bb43e5a685485c2983bcf
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-16 15:20:45 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.py
+++ 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.py
@@ -1,15 +1,24 @@
+from collections import Counter
+from typing import List
+
 class Solution:
     def minGroupsForValidAssignment(self, balls: List[int]) -> int:
         count = Counter(balls)
-        res = float('inf')
-        for k in range(1, min(count.values()) + 1):
+        minf = min(count.values())
+
+        for k in range(minf, 0, -1):
             boxes = 0
+            ok = True
+
             for f in count.values():
                 mn = (f + k) // (k + 1)
                 mx = f // k
+
                 if mn > mx:
+                    ok = False
                     break
+
                 boxes += mn
-            else:
-                res = min(res, boxes)
-        return res
+
+            if ok:
+                return boxes
