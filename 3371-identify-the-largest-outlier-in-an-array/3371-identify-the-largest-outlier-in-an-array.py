3deae77c584a3e813ede3dc5ff462f3ff56621ff
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-17 17:21:09 +0700

    Time: 80 ms (97.72%), Space: 30.9 MB (30.14%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
@@ -7,6 +7,6 @@ class Solution:
             if (s - b) % 2 != 0:
                 continue
             a = (s - b) // 2
-            if a == b and f >= 2 or a != b and a in count:
+            if f >= 2 if a == b else a in count:
                 res = max(res, b)
         return res
