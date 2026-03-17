b1d015c456a341a8d26e1617b35347b4a0496bc2
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-17 14:02:18 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2735-collecting-chocolates/2735-collecting-chocolates.py
+++ 2735-collecting-chocolates/2735-collecting-chocolates.py
@@ -1,12 +1,12 @@
 class Solution:
     def minCost(self, nums: List[int], x: int) -> int:
         n = len(nums)
-        dp = nums[:]  
+        min_cost = nums[:]  
         res = sum(nums)
 
         for ops in range(1, n):
             for i in range(n):
-                dp[i] = min(dp[i], nums[(i + ops) % n])
-            res = min(res, sum(dp) + ops * x)
+                min_cost[i] = min(min_cost[i], nums[(i + ops) % n])
+            res = min(res, sum(min_cost) + ops * x)
 
         return res
