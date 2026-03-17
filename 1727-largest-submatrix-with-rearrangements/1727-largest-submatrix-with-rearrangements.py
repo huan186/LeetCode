7b22cee7901613525b17fdc8b403733a8ec3e7ac
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-17 12:01:55 +0700

    Time: 103 ms (98.92%), Space: 33.7 MB (92.47%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -1,20 +1,13 @@
 class Solution:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        m, n = len(matrix), len(matrix[0])
+        n = len(matrix[0])
         height = [0] * n
         res = 0
-        
         for row in matrix:
             for i in range(n):
-                height[i] = height[i] + 1 if row[i] == 1 else 0
-            
-            count = [0] * (m + 1)
-            for h in height:
-                count[h] += 1
-            
-            width = 0
-            for h in range(m, 0, -1):
-                width += count[h]
-                res = max(res, width * h)
-        
+                height[i] = 0 if row[i] == 0 else height[i] + 1
+            temp = height[:]
+            temp.sort()
+            for i in range(n):
+                res = max(res, (n - i) * temp[i])
         return res
