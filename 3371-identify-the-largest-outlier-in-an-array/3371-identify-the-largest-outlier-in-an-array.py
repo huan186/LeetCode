6b1ab8c002daeb0626e6b653b406764132da4cf1
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-17 17:23:44 +0700

    Time: 79 ms (98.17%), Space: 30 MB (87.67%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
@@ -3,10 +3,9 @@ class Solution:
         s = sum(nums)
         count = Counter(nums)
         res = float('-inf')
-        for b, f in count.items():
-            if (s - b) % 2 != 0:
-                continue
-            a = (s - b) // 2
-            if f >= 2 if a == b else a in count:
-                res = max(res, b)
+        for a in count:
+            b = s - 2 * a
+            if b in count:
+                if a != b or count[a] >= 2:
+                    res = max(res, b)
         return res
