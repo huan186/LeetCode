0b4b86764794210dc3e6b02ebdd614fbb6f0fb9e
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-19 11:25:05 +0700

    Time: 106 ms (84.75%), Space: 42.7 MB (81.36%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.py
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -2,22 +2,19 @@ class Solution:
     def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         mod = 12345
         m, n = len(grid), len(grid[0])
-        left = [[1] * n for _ in range(m)]
-        right = [[1] * n for _ in range(m)]
+        
+        res = [[1] * n for _ in range(m)]
+        
+        pref = 1
         for i in range(m):
             for j in range(n):
-                if i == j == 0:
-                    continue
-                pi, pj = (i, j - 1) if j > 0 else (i - 1, n - 1)
-                left[i][j] = (left[pi][pj] * grid[pi][pj]) % mod
+                res[i][j] = pref
+                pref = (pref * grid[i][j]) % mod
+        
+        suff = 1
         for i in range(m - 1, -1, -1):
             for j in range(n - 1, -1, -1):
-                if i == m - 1 and j == n - 1:
-                    continue
-                ni, nj = (i, j + 1) if j < n - 1 else (i + 1, 0)
-                right[i][j] = (right[ni][nj] * grid[ni][nj]) % mod
-        res = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                res[i][j] = (left[i][j] * right[i][j]) % mod
+                res[i][j] = (res[i][j] * suff) % mod
+                suff = (suff * grid[i][j]) % mod
+        
         return res
