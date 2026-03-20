d4c3d9142b9091062909773c882cd715ec74ba36
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-20 16:31:45 +0700

    Time: 4 ms (37.52%), Space: 20.6 MB (5.83%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,29 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n <= 2:
+            return True
+        if n == 3:
+            return nums[1] <= nums[0] + nums[2]
+        nums = tuple(nums)
+        @lru_cache(None)
+        def max_score(x):
+            l = len(x)
+            if l == 1:
+                return x[0], 0
+            if l == 2:
+                return max(x), min(x)
+            # take left
+            s2l, s1l = max_score(x[1:])
+            s1l += x[0]
+
+            # take right
+            s2r, s1r = max_score(x[:-1])
+            s1r += x[-1]
+
+            if s1l >= s1r:
+                return s1l, s2l
+            return s1r, s2r
+
+        s1, s2 = max_score(nums)
+        return s1 >= s2
