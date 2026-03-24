c5d24051b17af6f86e51d76068df3cc880d0d3f7
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-24 23:00:55 +0700

    Time: 0 ms (100%), Space: 19.3 MB (88.72%) - LeetHub

---
+++ 1073-adding-two-negabinary-numbers/1073-adding-two-negabinary-numbers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addNegabinary(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        i, j = len(arr1) - 1, len(arr2) - 1
+        carry = 0
+        res = []
+
+        while i >= 0 or j >= 0 or carry:
+            a = arr1[i] if i >= 0 else 0
+            b = arr2[j] if j >= 0 else 0
+            total = a + b + carry
+            res.append(total & 1)
+            carry = -(total >> 1)
+            i -= 1
+            j -= 1
+
+        while len(res) > 1 and res[-1] == 0:
+            res.pop()
+
+        return res[::-1]
