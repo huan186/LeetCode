f9c0e81fec37a0e0ab4bca865f927356eaa8993a
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-24 17:36:10 +0700

    Time: 287 ms (56.5%), Space: 23 MB (30.19%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+import bisect
+
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         indices = defaultdict(list)
@@ -8,9 +11,12 @@ class Solution:
             idx = -1
             for ch in w:
                 idx_list = indices[ch]
-                if not idx_list or idx_list[-1] < idx:
+                if not idx_list:
                     return False
-                idx = idx_list[bisect.bisect_left(idx_list, idx)] + 1
+                pos = bisect.bisect_right(idx_list, idx)
+                if pos == len(idx_list):
+                    return False
+                idx = idx_list[pos]
             return True
 
         return sum(is_subsequence(word) for word in words)
