9980ea7d9e9822d9ae71b48bd41d3ea6f7f1b573
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-25 09:23:07 +0700

    Time: 87 ms (93.84%), Space: 43.8 MB (62.33%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -2,5 +2,5 @@ class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
         total = sum(chain(*grid))
         def helper(x):
-            return any((s << 1) == total for s in accumulate(sum(row) for row in x))
+            return any(s * 2 == total for s in accumulate(map(sum, x)))
         return helper(grid) or helper(zip(*grid))
