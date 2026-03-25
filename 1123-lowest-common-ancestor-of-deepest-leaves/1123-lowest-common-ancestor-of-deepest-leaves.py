a28186cdc448aa17146296bae25f4084959fa470
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-25 11:40:58 +0700

    Time: 3 ms (48.6%), Space: 19.7 MB (30.65%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,15 +1,16 @@
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root or not root.left and not root.right:
-            return root
-        def depth(node, d):
+        res = None
+        max_depth = -1
+        def dfs(node, d):
+            nonlocal res, max_depth
             if not node:
+                max_depth = max(d, max_depth)
                 return d
-            return max(depth(node.left, d + 1), depth(node.right, d + 1))
-        left = depth(root.left, 0)
-        right = depth(root.right, 0)
-        if left == right:
-            return root
-        if left > right:
-            return self.lcaDeepestLeaves(root.left)
-        return self.lcaDeepestLeaves(root.right)
+            l = dfs(node.left, d + 1)
+            r = dfs(node.right, d + 1)
+            if l == r == max_depth:
+                res = node
+            return max(l, r)
+        dfs(root, 0)
+        return res
