947763e136468bac3c86393fd59b5f1b6facd12d
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-25 09:04:51 +0700

    Time: 67 ms (97.95%), Space: 43.1 MB (70.55%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -1,25 +1,14 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        m, n = len(grid), len(grid[0])
-        sum_row = [0] * m
-        sum_col = [0] * n
-        total = 0
-        for i in range(m):
-            for j in range(n):
-                sum_row[i] += grid[i][j]
-                sum_col[j] += grid[i][j]
-                total += grid[i][j]
-        if total % 2 == 1:
-            return False
-        target = total // 2
-        def can_divide(arr):
-            curr_sum = 0
-            for i in range(len(arr) - 1):
-                curr_sum += arr[i]
-                if curr_sum == target:
+        total = sum(sum(row) for row in grid)
+        def helper(x):
+            s = 0
+            for row in x:
+                s += sum(row)
+                d = (s << 1) - total
+                if d == 0:
                     return True
-                if curr_sum > target:
+                if d > 0:
                     return False
             return False
-        
-        return can_divide(sum_row) or can_divide(sum_col)
+        return helper(grid) or helper(zip(*grid))
