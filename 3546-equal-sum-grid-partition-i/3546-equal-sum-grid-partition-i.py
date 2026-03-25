af562f810ba54df701b14e71cf2f6384564d98af
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-25 09:01:38 +0700

    Time: 207 ms (25.34%), Space: 41.6 MB (80.14%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -9,13 +9,17 @@ class Solution:
                 sum_row[i] += grid[i][j]
                 sum_col[j] += grid[i][j]
                 total += grid[i][j]
-
+        if total % 2 == 1:
+            return False
+        target = total // 2
         def can_divide(arr):
             curr_sum = 0
             for i in range(len(arr) - 1):
                 curr_sum += arr[i]
-                if (curr_sum << 1) == total:
+                if curr_sum == target:
                     return True
+                if curr_sum > target:
+                    return False
             return False
         
         return can_divide(sum_row) or can_divide(sum_col)
