02c4abc0b032141b3a5c10c6ed4046f4da842c39
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-26 10:00:01 +0700

    Time: 597 ms (97.22%), Space: 61.1 MB (16.67%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
-        rows = defaultdict(list)
-        cols = defaultdict(list)
+        pos = defaultdict(list)
         s = 0
         for i, row in enumerate(grid):
             for j, num in enumerate(row):
-                rows[num].append((i, j))
-                cols[num].append((j, i))
+                pos[num].append((i, j))
                 s += num
 
-        def helper(x, dct):
+        def helper(x, p):
             m, n = len(x), len(x[0])
             s1 = 0
             for i, row in enumerate(x[:-1]):
@@ -26,13 +24,13 @@ class Solution:
                             return True
                 else:
                     if diff > 0:
-                        l = dct[diff]
-                        if l and l[0][0] <= i and (l[0][1] == 0 or l[0][1] == n - 1 or i > 0):
+                        l = pos[diff]
+                        if l and l[0][p] <= i and (l[0][1 - p] == 0 or l[0][1 - p] == n - 1 or i > 0):
                             return True
                     else:
-                        l2 = dct[-diff]
-                        if l2 and l2[-1][0] > i and (l2[-1][1] == 0 or l2[-1][1] == n - 1 or i < m - 2):
+                        l2 = pos[-diff]
+                        if l2 and l2[-1][p] > i and (l2[-1][1 - p] == 0 or l2[-1][1 - p] == n - 1 or i < m - 2):
                             return True
             return False
 
-        return helper(grid, rows) or helper(list(zip(*grid)), cols)
+        return helper(grid, 0) or helper(list(zip(*grid)), 1)
