2f3a30761c724562d3a6d0ed137dc7e6e42519dc
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-26 16:41:49 +0700

    Time: 44 ms (91.54%), Space: 37.9 MB (95.38%) - LeetHub

--- 2766-relocate-marbles/2766-relocate-marbles.py
+++ 2766-relocate-marbles/2766-relocate-marbles.py
@@ -2,6 +2,6 @@ class Solution:
     def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
         res = set(nums)
         for from_pos, to_pos in zip(moveFrom, moveTo):
-            res.remove(from_pos)
+            res.discard(from_pos)
             res.add(to_pos)
         return sorted(res)
