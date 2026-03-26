ba50b200fe943cf2b7b70d2c18169562f4fadd49
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-26 10:42:45 +0700

    Time: 639 ms (83.33%), Space: 91.7 MB (8.33%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
@@ -4,10 +4,7 @@ class Solution:
         s = 0
         for i, row in enumerate(grid):
             for j, num in enumerate(row):
-                if len(pos[num]) < 2:
-                    pos[num].append((i, j))
-                else:
-                    pos[num][-1] = (i, j)
+                pos[num].append((i, j))
                 s += num
 
         def helper(x, p):
@@ -16,24 +13,17 @@ class Solution:
             for i, row in enumerate(x[:-1]):
                 s1 += sum(x[i])
                 diff = 2 * s1 - s
-                if diff == 0:
+                l1, l2 = pos[diff], pos[-diff]
+                if (
+                        diff == 0
+                        or (n == 1 and (
+                                (diff > 0 and (x[0][0] == diff or x[i][0] == diff))
+                                or (diff < 0 and (x[i + 1][0] == -diff or x[-1][0] == -diff)))
+                        ) or (n != 1 and (
+                                    (diff > 0 and l1 and l1[0][p] <= i and (l1[0][1 - p] == 0 or l1[0][1 - p] == n - 1 or i > 0))
+                                or (diff < 0 and l2 and l2[-1][p] > i and (l2[-1][1 - p] == 0 or l2[-1][1 - p] == n - 1 or i < m - 2))))
+                ):
                     return True
-                if n == 1:
-                    if diff > 0:
-                        if x[0][0] == diff or x[i][0] == diff:
-                            return True
-                    else:
-                        if x[i + 1][0] == -diff or x[-1][0] == -diff:
-                            return True
-                else:
-                    if diff > 0:
-                        l = pos[diff]
-                        if l and l[0][p] <= i and (l[0][1 - p] == 0 or l[0][1 - p] == n - 1 or i > 0):
-                            return True
-                    else:
-                        l2 = pos[-diff]
-                        if l2 and l2[-1][p] > i and (l2[-1][1 - p] == 0 or l2[-1][1 - p] == n - 1 or i < m - 2):
-                            return True
             return False
 
         return helper(grid, 0) or helper(list(zip(*grid)), 1)
