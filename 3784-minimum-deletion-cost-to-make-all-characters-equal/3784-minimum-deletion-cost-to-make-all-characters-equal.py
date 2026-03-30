e52883807906438db10aa4cd357cdc6816d349c0
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-30 13:03:05 +0700

    Time: 61 ms (92.46%), Space: 32.4 MB (92.74%) - LeetHub

--- 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal.py
+++ 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal.py
@@ -1,8 +1,6 @@
 class Solution:
     def minCost(self, s: str, cost: List[int]) -> int:
         costs = defaultdict(int)
-        total_cost = 0
         for ch, c in zip(s, cost):
             costs[ch] += c
-            total_cost += c
-        return min(total_cost - c for c in costs.values())
+        return sum(cost) - max(costs.values())
