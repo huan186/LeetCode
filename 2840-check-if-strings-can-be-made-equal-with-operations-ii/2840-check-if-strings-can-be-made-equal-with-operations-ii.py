773c79bc8c737eb5bedb04745b7368d39953b542
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-30 09:09:07 +0700

    Time: 239 ms (7.46%), Space: 19.9 MB (95.52%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        count_even = [0] * 26
-        count_odd = [0] * 26
+        count_even = Counter()
+        count_odd = Counter()
         i = 0
         for i, (c1, c2) in enumerate(zip(s1, s2)):
-            idx1, idx2 = ord(c1) - 97, ord(c2) - 97
             if i % 2 == 0:
-                count_even[idx1] += 1
-                count_even[idx2] -= 1
+                count_even[c1] += 1
+                count_even[c2] -= 1
             else:
-                count_odd[idx1] += 1
-                count_odd[idx2] -= 1
-        return all(c == 0 for c in (count_even + count_odd))
+                count_odd[c1] += 1
+                count_odd[c2] -= 1
+        def ok(count):
+            return all(f == 0 for f in count.values())
+        return ok(count_even) and ok(count_odd)
