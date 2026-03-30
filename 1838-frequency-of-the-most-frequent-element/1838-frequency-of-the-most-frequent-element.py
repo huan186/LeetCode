2de162fa8072920358ca9d58873fa1b9efd10b92
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-30 11:38:23 +0700

    Time: 254 ms (69.06%), Space: 30.3 MB (95.51%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        i, j, n = 0, 1, len(nums)
+        nums.sort()
+        n = len(nums)
+        i, j = n - 1, n - 2
         res = 1
-        while i < n - 1:
-            while j < n and k >= 0 and nums[i] - nums[j] <= k:
+        while i > 0 and j >= 0:
+            while j >= 0 and k >= 0 and nums[i] - nums[j] <= k:
                 k -= nums[i] - nums[j]
-                j += 1
-            cnt = j - i
-            k += (cnt - 1) * (nums[i] - nums[i + 1])
+                j -= 1
+            cnt = i - j
             res = max(res, cnt)
-            i += 1
-            if j <= i:
-                j = i + 1
+            k += (cnt - 1) * (nums[i] - nums[i - 1])
+            i -= 1
         return res
