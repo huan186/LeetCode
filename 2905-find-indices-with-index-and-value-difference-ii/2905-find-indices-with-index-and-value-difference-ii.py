7f98984e779fb6f64cdf81bea2c21fc69355a0d9
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-30 17:59:57 +0700

    Time: 32 ms (96.38%), Space: 33.2 MB (94.2%) - LeetHub

--- 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.py
+++ 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.py
@@ -1,21 +1,19 @@
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        n = len(nums)
-        min_indices = [0] * n
-        max_indices = [0] * n
-        for i in range(1, n):
-            if nums[i] > nums[max_indices[i - 1]]:
-                max_indices[i] = i
-            else:
-                max_indices[i] = max_indices[i - 1]
-            if nums[i] < nums[min_indices[i - 1]]:
-                min_indices[i] = i
-            else:
-                min_indices[i] = min_indices[i - 1]
-        for i in range(indexDifference, n):
+        min_idx = 0
+        max_idx = 0
+        
+        for i in range(indexDifference, len(nums)):
             j = i - indexDifference
-            if abs(nums[max_indices[j]] - nums[i]) >= valueDifference:
-                return [max_indices[j], i]
-            if abs(nums[min_indices[j]] - nums[i]) >= valueDifference:
-                return [min_indices[j], i]
+
+            if nums[j] < nums[min_idx]:
+                min_idx = j
+            if nums[j] > nums[max_idx]:
+                max_idx = j
+            
+            if nums[max_idx] - nums[i] >= valueDifference:
+                return [max_idx, i]
+            if nums[i] - nums[min_idx] >= valueDifference:
+                return [min_idx, i]
+        
         return [-1, -1]
