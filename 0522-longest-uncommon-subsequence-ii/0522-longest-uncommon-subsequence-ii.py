0f619126eed776bf53b945eff71037e2ab9654cf
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-31 14:47:21 +0700

    Time: 3 ms (40.11%), Space: 19.2 MB (82.42%) - LeetHub

---
+++ 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        def is_a_subset(x, y):
+            a, b = len(x), len(y)
+            if a > b:
+                return False
+            i1, i2= 0, 0
+            while i1 < a and i2 < b:
+                if x[i1] == y[i2]:
+                    i1 += 1
+                    i2 += 1
+                else:
+                    i2 += 1
+            return i1 == a
+        res = -1
+        n = len(strs)
+        seen = set()
+        for i in range(n):
+            if strs[i] in seen:
+                continue
+            for j in range(n):
+                if i == j:
+                    continue
+                if is_a_subset(strs[i], strs[j]):
+                    break
+            else:
+                res = max(res, len(strs[i]))
+            seen.add(strs[i])
+        return res
