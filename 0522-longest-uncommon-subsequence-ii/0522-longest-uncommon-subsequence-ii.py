446aa9e4f9b2e68a49ca392193355aa3ab2e11cf
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-31 14:48:32 +0700

    Time: 3 ms (40.11%), Space: 19.2 MB (96.7%) - LeetHub

--- 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.py
+++ 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.py
@@ -1,29 +1,16 @@
 class Solution:
     def findLUSlength(self, strs: List[str]) -> int:
-        def is_a_subset(x, y):
-            a, b = len(x), len(y)
-            if a > b:
-                return False
-            i1, i2 = 0, 0
-            while i1 < a and i2 < b:
-                if x[i1] == y[i2]:
-                    i1 += 1
-                    i2 += 1
-                else:
-                    i2 += 1
-            return i1 == a
-        res = -1
-        n = len(strs)
-        seen = set()
-        for i in range(n):
-            if strs[i] in seen:
-                continue
-            for j in range(n):
-                if i == j:
-                    continue
-                if is_a_subset(strs[i], strs[j]):
-                    break
-            else:
-                res = max(res, len(strs[i]))
-            seen.add(strs[i])
-        return res
+        def is_subseq(a, b):
+            i = 0
+            for c in b:
+                if i < len(a) and a[i] == c:
+                    i += 1
+            return i == len(a)
+
+        strs.sort(key=len, reverse=True)
+
+        for i, s in enumerate(strs):
+            if all(not is_subseq(s, t) for j, t in enumerate(strs) if i != j):
+                return len(s)
+
+        return -1
