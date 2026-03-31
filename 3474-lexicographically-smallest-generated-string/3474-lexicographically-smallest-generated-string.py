50f02dfa2289cfc221f8fda1ac0290e0a6e5db10
huan186 <131932368+huan186@users.noreply.github.com>
2026-03-31 10:19:49 +0700

    Time: 51 ms (92.31%), Space: 19.5 MB (51.28%) - LeetHub

--- 3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.py
+++ 3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.py
@@ -2,27 +2,23 @@ class Solution:
     def generateString(self, str1: str, str2: str) -> str:
         n, m = len(str1), len(str2)
         arr2 = list(str2)
-        res = [''] * (m + n - 1)
+        res = ['a'] * (m + n - 1)
         replaceable = [True] * (m + n - 1)
         for i, c in enumerate(str1):
             if c == 'T':
                 for j in range(m):
-                    if res[i + j] == '':
+                    if replaceable[i + j]:
                         res[i + j] = str2[j]
                     elif res[i + j] != str2[j]:
                         return ''
                     replaceable[i + j] = False
-        for i, c in enumerate(res):
-            if c == '':
-                res[i] = 'a'
         for i, c in enumerate(str1):
-            if c == 'T' or res[i : i + m] != arr2:
-                continue
-            for j in range(i + m - 1, i - 1, -1):
-                if replaceable[j]:
-                    res[j] = 'b'
-                    break
-            else:
-                return ''
+            if c == 'F' and res[i : i + m] == arr2:
+                for j in range(i + m - 1, i - 1, -1):
+                    if replaceable[j]:
+                        res[j] = 'b'
+                        break
+                else:
+                    return ''
 
         return ''.join(res)
