4e1c98ce72e207cebd6edfbfddfcda676b705d8a
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 12:50:03 +0700

    Time: 2225 ms (25.08%), Space: 171.8 MB (50%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.py
+++ 0464-can-i-win/0464-can-i-win.py
@@ -5,15 +5,18 @@ class Solution:
 
         if (maxChoosableInteger * (maxChoosableInteger + 1)) // 2 < desiredTotal:
             return False
-
-        @lru_cache(None)
+        memo = {}
         def win(state):
+            if state in memo:
+                return memo[state]
             for num in range(maxChoosableInteger, 0, -1):
                 mask = 1 << (num + 8)
                 if state & mask:
                     continue
                 if num >= (state & 511) or not win((state - num) | mask):
+                    memo[state] = True
                     return True
+            memo[state] = False
             return False
         
         return win(desiredTotal)
