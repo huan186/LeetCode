2611a9bff526fa3ddd8ed7a49a6847ead954fc8b
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 12:00:00 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,22 +1,15 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freq = [[0, chr(97 + i)] for i in range(26)]
-        for c in s:
-            freq[ord(c) - ord('a')][0] += 1
-        freq.sort(reverse=True)
-        f_max = freq[0][0]
-        n = len(s)
-        if 2 * f_max > n + 1:
+        counts, n = Counter(s), len(s)
+        if 2 * max(counts.values()) > n + 1:
             return ''
         res = [''] * n
         i = 0
-        for f, c in freq:
-            if f == 0:
-                break
+        for c, f in counts.items():
             while f > 0:
                 res[i] = c
+                f -= 1
                 i += 2
                 if i >= n:
                     i = 1
-                f -= 1
         return ''.join(res)
