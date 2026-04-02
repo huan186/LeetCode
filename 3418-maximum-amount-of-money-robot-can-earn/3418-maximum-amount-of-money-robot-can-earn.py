24bfecbd1951a1d69f5fd6ab30fe91048d883ddd
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 11:24:31 +0700

    Time: 5360 ms (13.46%), Space: 160.8 MB (18.27%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
@@ -1,27 +1,27 @@
 class Solution:
     def maximumAmount(self, coins: List[List[int]]) -> int:
         m, n = len(coins), len(coins[0])
-        inf = float('-inf')
+        inf = - 10 ** 18
         dp = [[[inf] * 3 for _ in range(n)] for _ in range(m)]
-        def dfs(row, col, k):
+
+        def dfs(row, col, k: int) -> int:
             if dp[row][col][k] != inf:
                 return dp[row][col][k]
             profit = inf
             if row == m - 1 and col == n - 1:
-                if coins[-1][-1] < 0 and k > 0:
+                if coins[-1][-1] < 0 < k:
                     profit = 0
                 else:
                     profit = coins[-1][-1]
             else:
-                profit = inf
                 for dr, dc in ((1, 0), (0, 1)):
                     nr, nc = row + dr, col + dc
                     if nr >= m or nc >= n:
                         continue
                     profit = max(profit, coins[row][col] + dfs(nr, nc, k))
-                    if coins[row][col] < 0 and k > 0:
+                    if coins[row][col] < 0 < k:
                         profit = max(profit, dfs(nr, nc, k - 1))
             dp[row][col][k] = profit
             return profit
-        
+
         return dfs(0, 0, 2)
