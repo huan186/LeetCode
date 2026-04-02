e325948b21675196a2b4411b081f4a176b606b8c
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 12:38:02 +0700

    Time: 2503 ms (19.8%), Space: 258 MB (8.51%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.py
+++ 0464-can-i-win/0464-can-i-win.py
@@ -1,8 +1,5 @@
 class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
-        if desiredTotal <= maxChoosableInteger:
-            return True
-
         if (maxChoosableInteger * (maxChoosableInteger + 1)) // 2 < desiredTotal:
             return False
 
