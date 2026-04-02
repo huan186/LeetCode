5754177572e6da84e1e13b6390803473ab375ba5
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 16:47:21 +0700

    Time: 38 ms (51.09%), Space: 22.1 MB (58.32%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,15 +1,15 @@
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        r = [(q / w, q) for q, w in zip(quality, wage)]
-        r.sort(reverse=True)
+        r = [(w / q, q) for q, w in zip(quality, wage)]
+        r.sort()
         total_quality = 0
         res = float('inf')
         heap = []
-        for i, (x, q) in enumerate(r): # x = quality / wage
+        for i, (x, q) in enumerate(r):
             if len(heap) == k:
                 total_quality += heapq.heappop(heap)
             total_quality += q
             heapq.heappush(heap, -q)
             if len(heap) == k:
-                res = min(res, total_quality / x)
+                res = min(res, total_quality * x)
         return res
