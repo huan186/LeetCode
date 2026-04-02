57bb019ebc7a405ccafcd1c86e2bc2fa18d00db9
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 17:04:56 +0700

    Time: 3 ms (31%), Space: 19.3 MB (48.94%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,18 +1,8 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         m, n = len(matrix), len(matrix[0])
-        def valid(r, c):
-            v = matrix[r][c]
-            while r < m and c < n:
-                if matrix[r][c] != v:
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != matrix[i-1][j-1]:
                     return False
-                r += 1
-                c += 1
-            return True
-        for i in range(m - 1):
-            if not valid(i, 0):
-                return False
-        for j in range(1, n - 1):
-            if not valid(0, j):
-                return False
         return True
