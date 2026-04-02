fb633553804670e6184c37f5c3f809c44b49b96a
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-02 11:33:28 +0700

    Time: 643 ms (100%), Space: 45.3 MB (100%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.py
@@ -2,26 +2,14 @@ class Solution:
     def maximumAmount(self, coins: List[List[int]]) -> int:
         m, n = len(coins), len(coins[0])
         inf = - 10 ** 18
-        dp = [[[inf] * 3 for _ in range(n)] for _ in range(m)]
-
-        def dfs(row, col, k: int) -> int:
-            if dp[row][col][k] != inf:
-                return dp[row][col][k]
-            profit = inf
-            if row == m - 1 and col == n - 1:
-                if coins[-1][-1] < 0 < k:
-                    profit = 0
-                else:
-                    profit = coins[-1][-1]
-            else:
-                for dr, dc in ((1, 0), (0, 1)):
-                    nr, nc = row + dr, col + dc
-                    if nr >= m or nc >= n:
-                        continue
-                    profit = max(profit, coins[row][col] + dfs(nr, nc, k))
-                    if coins[row][col] < 0 < k:
-                        profit = max(profit, dfs(nr, nc, k - 1))
-            dp[row][col][k] = profit
-            return profit
-
-        return dfs(0, 0, 2)
+        dp = [[inf] * 3 for _ in range(n + 1)]
+        dp[1] = [0] * 3
+        for i in range(m):
+            for j in range(n):
+                coin = coins[i][j]
+                up0, up1, up2 = dp[j + 1]
+                left0, left1, left2 = dp[j]
+                dp[j + 1][0] = max(up0, left0) + coin
+                dp[j + 1][1] = max(up1 + coin, left1 + coin, up0, left0)
+                dp[j + 1][2] = max(up2 + coin, left2 + coin, up1, left1)
+        return dp[-1][-1]
