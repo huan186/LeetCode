baf20e911a07a4bd7ae921b9a374300688374c90
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-03 10:20:35 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxWalls(self, robots: List[int], distance: List[int], walls: List[int]) -> int:
+        z = sorted(zip(robots, distance))
+        walls.sort()
+        left, right, ri = 0, 0, 0
+        for i, (robot, dist) in enumerate(z):
+            prev_robot = 0 if i == 0 else z[i - 1][0]
+            next_robot = 10 ** 18 if i == len(z) - 1 else z[i + 1][0]
+            l = bisect.bisect_left(walls, max(prev_robot + 1, robot - dist))
+            ml = bisect.bisect_right(walls, robot)
+            mr = bisect.bisect_left(walls, robot)
+            r = bisect.bisect_right(walls, min(next_robot - 1, robot + dist))
+            left = max(left + ml - l, right + max(l - 1, ri))
+            right = max(left, right) + r - mr
+            ri = r
+        return max(left, right)
