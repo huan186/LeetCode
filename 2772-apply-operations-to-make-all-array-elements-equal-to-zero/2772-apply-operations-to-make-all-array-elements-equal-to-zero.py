5ebcd873786802181a71039b8e08349e8e9b0054
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-03 17:18:45 +0700

    Time: 46 ms (65.29%), Space: 32.7 MB (84.3%) - LeetHub

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkArray(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        for i in range(n - k):
+            if i + k < n:
+                nums[i + k] += nums[i]
+            if i > 0 and nums[i] < nums[i - 1]:
+                return False
+        return len(set(nums[-1:-k - 1:-1])) == 1
