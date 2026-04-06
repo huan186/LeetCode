26b018cc4d5feab96c4520821a5f19cdc560ac06
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-06 14:02:22 +0700

    Time: 4 ms (87.1%), Space: 32.1 MB (88.71%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
@@ -1,6 +1,10 @@
 class Solution:
     def minCost(self, startPos: List[int], homePos: List[int], rowCosts: List[int], colCosts: List[int]) -> int:
-        dx = -1 if startPos[0] > homePos[0] else 1
-        dy = -1 if startPos[1] > homePos[1] else 1
-        return (sum(rowCosts[x] for x in range(startPos[0] + dx, homePos[0] + dx, dx)) +
-                sum(colCosts[y] for y in range(startPos[1] + dy, homePos[1] + dy, dy)))
+        sr, sc = startPos
+        hr, hc = homePos
+        return 0 if startPos == homePos else (
+            sum(rowCosts[min(sr, hr) + 1:max(sr, hr)]) +
+            sum(colCosts[min(sc, hc) + 1:max(sc, hc)]) +
+            (0 if sr == hr else rowCosts[hr]) +
+            (0 if sc == hc else colCosts[hc])
+        )
