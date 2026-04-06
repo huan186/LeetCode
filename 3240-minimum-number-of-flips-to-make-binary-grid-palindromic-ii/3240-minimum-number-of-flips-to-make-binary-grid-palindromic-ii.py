4ed145a35eb1bddf5653b9784872e439d9745bca
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-06 10:28:59 +0700

    Time: 456 ms (7.14%), Space: 55.1 MB (19.05%) - LeetHub

--- 3240-minimum-number-of-flips-to-make-binary-grid-palindromic-ii/3240-minimum-number-of-flips-to-make-binary-grid-palindromic-ii.py
+++ 3240-minimum-number-of-flips-to-make-binary-grid-palindromic-ii/3240-minimum-number-of-flips-to-make-binary-grid-palindromic-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def minFlips(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        res = c1 = 0
-        ok = False
+        res = pair1 = 0
+        flex = False
         for i in range((m + 1) // 2):
             for j in range((n + 1) // 2):
                 pos = {(i, j), (m - i - 1, j), (i, n - j - 1), (m - i - 1, n - j - 1)}
@@ -14,7 +14,7 @@ class Solution:
                     res += min(cnt1, l - cnt1)
                     if l == 2:
                         if cnt1 == 1:
-                            ok = True
+                            flex = True
                         elif cnt1 == 2:
-                            c1 += 2
-        return res if ok or c1 % 4 == 0 else res + 2
+                            pair1 += 2
+        return res if flex or pair1 % 4 == 0 else res + 2
