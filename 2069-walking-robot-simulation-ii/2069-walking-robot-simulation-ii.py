26e74b00d250cc49118d9c2b62cb75138eb22676
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-07 09:24:16 +0700

    Time: 32 ms (95.73%), Space: 23.6 MB (75.21%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.py
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.py
@@ -11,26 +11,30 @@ class Robot:
             self.perimeter
         ]
         self.pos = 0
+        self.moved = False
 
     def step(self, num: int) -> None:
         self.pos = (self.pos + num) % self.perimeter
+        self.moved = True
 
     def getPos(self) -> List[int]:
         pos = self.pos
-        if pos < self.x[0]:
+        if pos <= self.x[0]:
             return [pos, 0]
-        if pos < self.x[1]:
+        if pos <= self.x[1]:
             return [self.w - 1, pos - self.x[0]]
-        if pos < self.x[2]:
+        if pos <= self.x[2]:
             return [self.w - 1 - (pos - self.x[1]), self.h - 1]
         return [0, self.h - 1 - (pos - self.x[2])]
 
     def getDir(self) -> str:
         pos = self.pos
-        if pos < self.x[0]:
+        if self.pos == 0 and self.moved:
+            return "South"
+        if pos <= self.x[0]:
             return "East"
-        if pos < self.x[1]:
+        if pos <= self.x[1]:
             return "North"
-        if pos < self.x[2]:
+        if pos <= self.x[2]:
             return "West"
         return "South"
