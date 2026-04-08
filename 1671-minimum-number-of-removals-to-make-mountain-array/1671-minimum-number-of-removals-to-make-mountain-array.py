2ca209af3091485794add7f5595611e50c510e03
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-08 11:24:29 +0700

    Time: 16 ms (72.49%), Space: 19.6 MB (41.8%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,18 +1,20 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         n = len(nums)
-        inc = [1] * n
-        for i in range(1, n - 1):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    inc[i] = max(inc[i], inc[j] + 1)
-        dec = [1] * n
-        for i in range(n - 2, 0, -1):
-            for j in range(n - 1, i, -1):
-                if nums[j] < nums[i]:
-                    dec[i] = max(dec[i], dec[j] + 1)
-        max_len = 0
-        for i in range(1, n - 1):
-            if inc[i] != 1 and dec[i] != 1:
-                max_len = max(max_len, inc[i] + dec[i])
-        return n - max_len + 1
+        
+        def find_max_len(start: int, end: int, steps) -> List[int]:
+            tails = []
+            x = [0] * n
+            for i in range(start, end, steps):
+                pos = bisect.bisect_left(tails, nums[i])
+                if pos == len(tails):
+                    tails.append(nums[i])
+                else:
+                    tails[pos] = nums[i]
+                x[i] = pos + 1
+            return x
+        
+        inc = find_max_len(0, n, 1)
+        dec = find_max_len(n - 1, -1, -1)
+        
+        return n + 1 - max(inc[i] + dec[i] for i in range(1, n - 1) if inc[i] != 1 and dec[i] != 1)
