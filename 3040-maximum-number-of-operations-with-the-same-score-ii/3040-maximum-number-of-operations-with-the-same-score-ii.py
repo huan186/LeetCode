f2f1a58dc0808ceab6f129754e4f9e7042687d94
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-08 11:58:36 +0700

    Time: 1838 ms (62%), Space: 397.8 MB (46%) - LeetHub

---
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        @cache
+        def dfs(i: int, j: int, target: int) -> int:
+            if i >= j:
+                return 0
+            ops = 0
+            if nums[i] + nums[j] == target:
+                ops = max(ops, 1 + dfs(i + 1, j - 1, target))
+            if nums[i] + nums[i + 1] == target:
+                ops = max(ops, 1 + dfs(i + 2, j, target))
+            if nums[j] + nums[j - 1] == target:
+                ops = max(ops, 1 + dfs(i, j - 2, target))
+            return ops
+
+        res = 0
+
+        for s in {nums[0] + nums[1], nums[-1] + nums[-2], nums[0] + nums[-1]}:
+            res = max(res, dfs(0, n - 1, s))
+
+        return res
