1755ca6834a79c8ed317dce8830dc14f20d93025
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-08 12:03:11 +0700

    Time: 1925 ms (51%), Space: 395.3 MB (52%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def maxOperations(self, nums: List[int]) -> int:
         n = len(nums)
-
         @cache
         def dfs(i: int, j: int, target: int) -> int:
             if i >= j:
                 return 0
+            if i == j - 1:
+                return int(nums[i] + nums[j] == target)
             ops = 0
             if nums[i] + nums[j] == target:
                 ops = max(ops, 1 + dfs(i + 1, j - 1, target))
@@ -14,10 +15,7 @@ class Solution:
             if nums[j] + nums[j - 1] == target:
                 ops = max(ops, 1 + dfs(i, j - 2, target))
             return ops
-
         res = 0
-
         for s in {nums[0] + nums[1], nums[-1] + nums[-2], nums[0] + nums[-1]}:
             res = max(res, dfs(0, n - 1, s))
-
         return res
