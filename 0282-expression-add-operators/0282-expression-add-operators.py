6425b6baed27e957a79dd60a7ce9c606e333981a
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-09 14:03:21 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.py
@@ -0,0 +1,23 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        res = []
+        n = len(num)
+        def dfs(start, expression, value, last):
+            if start == n:
+                if value == target:
+                    res.append(expression)
+                return
+
+            for end in range(start, n):
+                if end > start and num[start] == '0':
+                    break
+                cur = int(num[start:end + 1])
+                if end == 0:
+                    dfs(end + 1, str(cur), cur, cur)
+                else:
+                    dfs(end + 1, expression + "+" + str(cur), value + cur, cur)
+                    dfs(end + 1, expression + "-" + str(cur), value - cur, -cur)
+                    dfs(end + 1, expression + "*" + str(cur), value - last + last * cur, last * cur)
+            
+        dfs(0, "", 0, 0)
+        return res
