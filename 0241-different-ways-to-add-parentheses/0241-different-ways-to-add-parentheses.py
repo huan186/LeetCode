7cad28c8c14b27e57c1b101398441f7ba8ca1697
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-09 17:54:34 +0700

    Time: 0 ms (100%), Space: 19.3 MB (74.51%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,25 +1,18 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        nums = []
-        ops = []
-        cur = 0
-        for c in expression:
-            if c.isdigit():
-                cur = cur * 10 + int(c)
-            else:
-                nums.append(cur)
-                ops.append(c)
-                cur = 0
-        nums.append(cur)
+    @cache
+    def diffWaysToCompute(self, expr: str) -> List[int]:
+        res = []
+        for i, c in enumerate(expr):
+            if c in "+-*":
+                left = self.diffWaysToCompute(expr[:i])
+                right = self.diffWaysToCompute(expr[i+1:])
 
-        def operate(o1, o2, op):
-            return o1 + o2 if op == '+' else o1 - o2 if op == '-' else o1 * o2
-
-        @cache
-        def compute(l, r):
-            return [operate(o1, o2, ops[i - 1])
-                    for i in range(l + 1, r + 1)
-                    for o1 in compute(l, i - 1)
-                    for o2 in compute(i, r)] if l < r else [nums[l]]
-
-        return compute(0, len(nums) - 1)
+                for l in left:
+                    for r in right:
+                        if c == '+':
+                            res.append(l + r)
+                        elif c == '-':
+                            res.append(l - r)
+                        else:
+                            res.append(l * r)
+        return res if res else [int(expr)]
