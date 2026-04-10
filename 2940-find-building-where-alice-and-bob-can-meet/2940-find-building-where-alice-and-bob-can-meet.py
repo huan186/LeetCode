3bcfe25ca7d1bb1461cc280a44ce38ff8c0febc0
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-10 11:24:31 +0700

    Time: 431 ms (52.01%), Space: 48.7 MB (29.3%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.py
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.py
@@ -1,43 +1,33 @@
 class Solution:
-    def leftmostBuildingQueries(self, heights, queries):
-        n = len(heights)
-        q = len(queries)
-        ans = [-1] * q
-
-        new_queries = [[] for _ in range(n)]
-
-        for i, (a, b) in enumerate(queries):
-            if a > b:
-                a, b = b, a
-            if a == b or heights[a] < heights[b]:
-                ans[i] = b
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        n, m = len(heights), len(queries)
+        res = [-1] * m
+        q = [[] for _ in range(n)]
+        for idx, (i, j) in enumerate(queries):
+            if i > j:
+                i, j = j, i
+            if i == j or heights[i] < heights[j]:
+                res[idx] = j
             else:
-                new_queries[b].append((heights[a], i))
+                q[j].append((heights[i], idx))
 
-        stack = []
-
-        for i in range(n - 1, -1, -1):
-            # process queries at i
-            for h, idx in new_queries[i]:
-                # binary search in stack
-                l, r = 0, len(stack) - 1
-                res = -1
-
+        st = []
+        for i in range(len(heights) - 1, -1, -1):
+            for h, idx in q[i]:
+                l, r = 0, len(st) - 1
+                ans = -1
                 while l <= r:
-                    mid = (l + r) // 2
-                    if heights[stack[mid]] > h:
-                        res = stack[mid]
-                        l = mid + 1
+                    m = (l + r) // 2
+                    if heights[st[m]] > h:
+                        ans = st[m]
+                        l = m + 1
                     else:
-                        r = mid - 1
-
-                if res != -1:
-                    ans[idx] = res
-
-            # maintain decreasing stack
-            while stack and heights[stack[-1]] <= heights[i]:
-                stack.pop()
-
-            stack.append(i)
-
-        return ans
+                        r = m - 1
+                if ans != -1:
+                    res[idx] = ans
+        
+            while st and heights[st[-1]] <= heights[i]:
+                st.pop()
+            st.append(i)
+        
+        return res
