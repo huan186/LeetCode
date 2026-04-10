62762163ed4d2f31ec1a8c2c7c5991565c8e588d
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-10 17:01:09 +0700

    Time: 3 ms (88.62%), Space: 19.6 MB (30.89%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.py
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.py
@@ -1,21 +1,8 @@
 class Solution:
     def maximumRows(self, matrix: List[List[int]], numSelect: int) -> int:
-        m, n = len(matrix), len(matrix[0])
-
-        res = 0
         nums = [int("".join(map(str, row)), 2) for row in matrix]
-
-        def max_rows(x):
-            nonlocal res
-            res = max(res, sum(num == (num & x) for num in nums))
-
-        def dfs(i, k, curr):
-            if k == 0:
-                max_rows(curr)
-                return
-            for j in range(i, n):
-                dfs(j + 1, k - 1, curr | (1 << j))
-
-        dfs(0, numSelect, 0)
-
+        res = 0
+        for comb in combinations(range(len(matrix[0])), numSelect):
+            mask = sum(1 << c for c in comb)
+            res = max(res, sum(num == num & mask for num in nums))
         return res
