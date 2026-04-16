7b1dad84d84ce940ece8e215861d6c3fa78d1109
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-16 13:47:35 +0700

    Time: 402 ms (37.5%), Space: 54.4 MB (80.47%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -1,8 +1,15 @@
 class Solution:
     def solveQueries(self, nums, queries):
         n = len(nums)
+
         def min_distance_between(a, b):
             return min(a - b, n + b - a) if a > b else min(b - a, n + a - b)
+
+        def find_min_distance(query, indices_list):
+            idx = bisect.bisect_left(indices_list, query)
+            return min(min_distance_between(indices_list[idx], indices_list[(idx + 1) % len(indices_list)]),
+                       min_distance_between(indices_list[idx], indices_list[idx - 1]))
+
         indices = dict()
         for i in range(0, len(nums)):
             list_indices = indices.get(nums[i], [])
@@ -13,11 +20,7 @@ class Solution:
             num = nums[query]
             list_indices = indices.get(num, [])
             if len(list_indices) > 1:
-                min_distance = 100000
-                list_indices = indices[num]
-                for index in list_indices:
-                    if index != query:
-                        min_distance = min(min_distance, min_distance_between(index, query))
+                min_distance = find_min_distance(query, list_indices)
                 result.append(min_distance)
             else:
                 result.append(-1)
