ba9165bb289e47aa21af6428d3d1c3a582eaf37e
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-16 15:32:49 +0700

    Time: 10 ms (64.02%), Space: 19.2 MB (97.05%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreDifference(self, nums: List[int]) -> int:
+        diff = 0
+        sign = 1
+        for i, num in enumerate(nums):
+            if nums[i] % 2:
+                sign *= -1
+            if i % 6 == 5:
+                sign *= -1
+            diff += num * sign
+        return diff
