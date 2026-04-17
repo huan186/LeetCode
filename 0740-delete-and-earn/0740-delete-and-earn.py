ca91a782570eebc093421e0e24bb805a555c12e7
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-17 14:41:46 +0700

    Time: 3 ms (93.92%), Space: 20.2 MB (99.69%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -6,6 +6,5 @@ class Solution:
             f[num] += 1
         take = skip = 0
         for i in range(1, m + 1):
-            take, skip = max(take, skip + i * f[i]), max(take, skip)
+            take, skip = skip + i * f[i], max(take, skip)
         return max(take, skip)
-            
