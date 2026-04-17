b296529c9b687dc58068ab5a2fd6d934f256f599
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-17 14:49:38 +0700

    Time: 26 ms (56.83%), Space: 19.6 MB (94.27%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -3,8 +3,6 @@ class Solution:
         res = []
         for denominator in range(2, n + 1):
             for numerator in range(1, denominator):
-                if numerator != 1 and gcd(denominator, numerator) != 1:
-                    continue
-                else:
+                if gcd(denominator, numerator) == 1:
                     res.append(str(numerator) + "/" + str(denominator))
         return res
