f6dcc1b0965dae2b62f189a195b7d93dec99f93e
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-17 10:34:12 +0700

    Time: 1 ms (8.28%), Space: 19.5 MB (6.4%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -3,27 +3,12 @@ class Solution:
 
         def is_valid_ipv4():
             parts = queryIP.split(".")
-            if len(parts) != 4:
-                return False
-            def valid(p):
-                if len(p) == 0 or len(p) > 3 or p[0] == '0' and len(p) > 1:
-                    return False
-                return all(c.isdigit() for c in p) and int(p) <= 255
-            return all(valid(p) for p in parts)
+            return len(parts) == 4 and not any(not p.isdigit() or p[0] == '0' and len(p) > 1 or int(p) > 255 for p in parts)
 
         def is_valid_ipv6():
             parts = queryIP.split(":")
-            if len(parts) != 8:
-                return False
-            def valid(p):
-                if len(p) == 0 or len(p) > 4:
-                    return False
-                for c in p.lower():
-                    if not c.isdigit() and ('a' > c or 'f' < c):
-                        return False
-                return True
-            return all(valid(p) for p in parts)
-
+            hexdigits = '0123456789abcdefABCDEF'
+            return len(parts) == 8 and not any(len(p) == 0 or len(p) > 4 or any(c not in hexdigits for c in p)for p in parts)
 
         if "." in queryIP:
             return "IPv4" if is_valid_ipv4() else "Neither"
