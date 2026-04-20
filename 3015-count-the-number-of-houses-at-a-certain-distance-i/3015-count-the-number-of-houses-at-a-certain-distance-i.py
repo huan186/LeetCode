bb1273d28a690c37637b61fe26d827a0ea4144f2
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-20 10:48:51 +0700

    Time: 65 ms (97.79%), Space: 19.4 MB (58.09%) - LeetHub

--- 3015-count-the-number-of-houses-at-a-certain-distance-i/3015-count-the-number-of-houses-at-a-certain-distance-i.py
+++ 3015-count-the-number-of-houses-at-a-certain-distance-i/3015-count-the-number-of-houses-at-a-certain-distance-i.py
@@ -1,12 +1,12 @@
 class Solution:
     def countOfPairs(self, n: int, x: int, y: int) -> List[int]:
+        x, y = min(x, y), max(x, y)
         res = [0] * n
         for i in range(1, n):
             for j in range(i + 1, n + 1):
                 d = min(
-                    abs(i - j),
-                    abs(i - x) + 1 + abs(j - y),
-                    abs(i - y) + 1 + abs(j - x),
+                    j - i,
+                    abs(i - x) + 1 + abs(j - y)
                 )
                 res[d - 1] += 2
         return res
