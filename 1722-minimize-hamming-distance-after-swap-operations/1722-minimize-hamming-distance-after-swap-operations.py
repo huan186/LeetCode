24a1a9d3d11d078f3e8951deec7d7094e171b178
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-21 15:48:11 +0700

    Time: 175 ms (94.74%), Space: 57.4 MB (67.67%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
+        n = len(source)
+        parents = list(range(n))
+
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px != py:
+                parents[py] = px
+                
+        for a, b in allowedSwaps:
+            union(a, b)
+
+        d = defaultdict(lambda: defaultdict(int))
+
+        for i in range(n):
+            p = find(i)
+            d[p][source[i]] += 1
+            d[p][target[i]] -= 1        
+        
+        return sum(abs(f) for x in d.values() for f in x.values()) // 2
