bd88c475b5eda8f9d6baede52f99d6b6016932ad
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-22 16:00:45 +0700

    Time: 8 ms (87.75%), Space: 19.2 MB (96.08%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,33 +1,18 @@
-class PrefixTree:
-    def __init__(self):
-        self.children = {}
-        self.is_end = False
-
-    def add(self, word):
-        curr = self
-        for ch in word:
-            if ch not in curr.children:
-                curr.children[ch] = PrefixTree()
-            curr = curr.children[ch]
-        curr.is_end = True
-
-    def search(self, word):
-        return self.__search(word, 0, self, 2)
-
-    def __search(self, word, i, node, remain):
-        if i == len(word):
-            return node.is_end
-
-        for ch in node.children:
-            nr = remain if ch == word[i] else remain - 1
-            if nr >= 0 and self.__search(word, i + 1, node.children[ch], nr):
-                return True
-
-        return False
-
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        prefix_dict = PrefixTree()
-        for word in dictionary:
-            prefix_dict.add(word)
-        return [q for q in queries if prefix_dict.search(q)]
+        res = []
+        for q in queries:
+            ok = False
+            for d in dictionary:
+                cnt = 0
+                for a, b in zip(q, d):
+                    if a != b:
+                        cnt += 1
+                    if cnt > 2:
+                        break
+                else:
+                    ok = True
+                    break
+            if ok:
+                res.append(q)
+        return res
