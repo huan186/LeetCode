41e272fcd5ac3f3c858446a3838c00974877ecf3
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-22 16:02:40 +0700

    Time: 131 ms (13.23%), Space: 19.4 MB (50.49%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,18 +1,6 @@
 class Solution:
-    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        res = []
-        for q in queries:
-            ok = False
-            for d in dictionary:
-                cnt = 0
-                for a, b in zip(q, d):
-                    if a != b:
-                        cnt += 1
-                    if cnt > 2:
-                        break
-                else:
-                    ok = True
-                    break
-            if ok:
-                res.append(q)
-        return res
+    def twoEditWords(self, queries, dictionary):
+        def ok(a, b):
+            return sum(x != y for x, y in zip(a, b)) <= 2
+        
+        return [q for q in queries if any(ok(q, d) for d in dictionary)]
