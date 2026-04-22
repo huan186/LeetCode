95b8b61fabdd019d80b05dd2b802b76c8ae8edba
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-22 16:47:43 +0700

    Time: 7 ms (93.26%), Space: 20.6 MB (52.98%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,23 +1,17 @@
 class Solution:
-    def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
+    def matrixBlockSum(self, mat, k):
         m, n = len(mat), len(mat[0])
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        ps = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(m):
             for j in range(n):
-                dp[i + 1][j + 1] = mat[i][j] + dp[i + 1][j]
-            for j in range(n):
-                dp[i + 1][j + 1] += dp[i][j + 1]
+                ps[i + 1][j + 1] = mat[i][j]+ ps[i][j + 1]+ ps[i + 1][j]- ps[i][j]
 
-        def calculate_sum(t, b, l, r):
-            t, b = max(t, 0), min(b, m - 1)
-            l, r = max(l, 0), min(r, n - 1)
-            return dp[b + 1][r + 1] + dp[t][l] - dp[b + 1][l] - dp[t][r + 1]
-        
         res = [[0] * n for _ in range(m)]
 
         for i in range(m):
-            top, bottom = i - k, i + k
             for j in range(n):
-                left, right = j - k, j + k
-                res[i][j] = calculate_sum(top, bottom, left, right)
+                r1, r2 = max(0, i - k), min(m, i + k + 1)
+                c1, c2= max(0, j - k), min(n, j + k + 1)
+                res[i][j] = ps[r2][c2] - ps[r1][c2] - ps[r2][c1] + ps[r1][c1]
+
         return res
