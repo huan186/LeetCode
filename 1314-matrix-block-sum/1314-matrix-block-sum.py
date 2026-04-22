37b2fb5971b0c9f644f838ea7e3af290ef7278cd
huan186 <131932368+huan186@users.noreply.github.com>
2026-04-22 16:48:05 +0700

    Time: 8 ms (83.55%), Space: 20.5 MB (73.32%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -4,7 +4,7 @@ class Solution:
         ps = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(m):
             for j in range(n):
-                ps[i + 1][j + 1] = mat[i][j]+ ps[i][j + 1]+ ps[i + 1][j]- ps[i][j]
+                ps[i + 1][j + 1] = mat[i][j] + ps[i][j + 1] + ps[i + 1][j] - ps[i][j]
 
         res = [[0] * n for _ in range(m)]
 
