3f2f4ea49dbb304d7f1e607e83bf39302326cbd8
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-15 10:57:28 +0700

    Time: 5 ms (54.07%), Space: 19.4 MB (52.09%) - LeetHub - Time: O(?), Space: O(?)

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
@@ -1,11 +1,19 @@
 class Solution:
     def isDigitorialPermutation(self, n: int) -> bool:
-        fact =[1] * 10
+        fact = [1] * 10
         for i in range(1, 10):
             fact[i] = fact[i - 1] * i
-        n = str(n)
-        sf = sum(fact[int(d)] for d in n)
-        return Counter(n) == Counter(str(sf))
+        freq = [0] * 10
+        sf = 0
+        while n:
+            d = n % 10
+            sf += fact[d]
+            freq[d] += 1
+            n //= 10
+        while sf:
+            freq[sf % 10] -= 1
+            sf //= 10
+        return not any(freq)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
