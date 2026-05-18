4266c1a586886a9a10590f7aed414a9a40091069
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-18 11:23:27 +0700

    Time: 212 ms (28.23%), Space: 34 MB (68.6%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n < 2:
+            return 0
+        idx = defaultdict(list)
+        for i, v in enumerate(arr):
+            idx[v].append(i)
+        q = deque([(0, 0)])
+        visited = [False] * n
+        visited[0] = True
+        while q:
+            steps, i = q.popleft()
+            v = arr[i]
+            for j in set(idx[v] + [i - 1, i + 1]):
+                if 0 < j < n and j != i and not visited[j]:
+                    if j == n - 1:
+                        return steps + 1
+                    q.append((steps + 1, j))
+                    visited[j] = True
+            idx[v] = []
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
