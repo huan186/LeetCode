64a73cbade4ef849de5d2fa295110ab3b70423ec
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-19 09:24:07 +0700

    Time: 11 ms (48.8%), Space: 37.7 MB (79.31%) - LeetHub - Time: O(?), Space: O(?)

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,7 +1,17 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        set1 = set(nums1)
-        for num in nums2:
-            if num in set1:
-                return num
+        i = j = 0
+        n, m = len(nums1), len(nums2)
+        while i < n and j < m:
+            diff = nums1[i] - nums2[j]
+            if diff == 0:
+                return nums1[i]
+            if diff > 0:
+                j += 1
+            else:
+                i += 1
         return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
