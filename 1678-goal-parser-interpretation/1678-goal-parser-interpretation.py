6baa66af7aae927236e627ff1257aff38ed1d810
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-19 14:12:00 +0700

    Time: 36 ms (93.56%), Space: 19.2 MB (44.46%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace('()', 'o').replace('(', '').replace(')', '')
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
