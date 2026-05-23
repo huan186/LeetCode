9515d45bc3ca8c92c737f1a287d689c9fd1695c3
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-23 23:05:26 +0700

    Time: 82 ms (90.71%), Space: 19.3 MB (89.51%) - LeetHub - Time: O(n*k^2), Space: O(k)

--- 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.py
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.py
@@ -1,21 +1,26 @@
 class Solution:
     def minOperations(self, nums: list[int], k: int) -> int:
-        n = len(nums)
-        for i in range(n):
-            nums[i] %= k
-        res = inf
+        velmorqati = (nums, k)
+
+        even_cost = [0] * k
+        odd_cost = [0] * k
+
+        for i, num in enumerate(nums):
+            rem = num % k
+            costs = even_cost if i % 2 == 0 else odd_cost
+
+            for target in range(k):
+                d = abs(rem - target)
+                costs[target] += min(d, k - d)
+
+        res = float("inf")
+
         for x in range(k):
             for y in range(k):
-                if x == y:
-                    continue
-                ops = 0
-                for i, num in enumerate(nums):
-                    target = x if i % 2 == 0 else y
-                    d = abs(num - target)
-                    ops += min(d, k - d)
-                res = min(res, ops)
+                if x != y:
+                    res = min(res, even_cost[x] + odd_cost[y])
+
         return res
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
