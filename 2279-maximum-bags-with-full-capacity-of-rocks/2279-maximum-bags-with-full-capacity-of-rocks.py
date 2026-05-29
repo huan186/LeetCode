529a08d62ae420da93fcfd7986ae125e06ccba4a
huan186 <131932368+huan186@users.noreply.github.com>
2026-05-29 14:50:15 +0700

    Time: 54 ms (79.05%), Space: 28.2 MB (72.38%) - LeetHub - Time: O(?), Space: O(?)

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,16 +1,16 @@
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
         n = len(capacity)
-        heap = [capacity[i] - rocks[i] for i in range(n)]
-        heapq.heapify(heap)
+        for i in range(n):
+            rocks[i] -= capacity[i]
+        rocks.sort(reverse=True)
         res = 0
-        while heap:
-            curr = heapq.heappop(heap)
-            if curr <= 0:
+        for r in rocks:
+            if r >= 0:
                 res += 1
-            elif additionalRocks >= curr:
+            elif additionalRocks + r >= 0:
                 res += 1
-                additionalRocks -= curr
+                additionalRocks += r
             else:
                 break
         return res
