802d73d449b13684946fa65ba48ae8bd4176cc95
huan186 <131932368+huan186@users.noreply.github.com>
2026-06-04 16:49:26 +0700

    Time: 0 ms (100%), Space: 32.3 MB (43.94%) - LeetHub - Time: O(?), Space: O(?)

--- 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.py
+++ 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.py
@@ -1,18 +1,9 @@
 class Solution:
     def minLengthAfterRemovals(self, nums: List[int]) -> int:
-        counts = Counter(nums)
-        heap = [(-v, k) for k, v in counts.items()]
-        heapq.heapify(heap)
-        c = 0
-        while len(heap) > 1:
-            v1, k1 = heapq.heappop(heap)
-            v2, k2 = heapq.heappop(heap)
-            c += 2
-            if v1 + 1 < 0:
-                heapq.heappush(heap, (v1 + 1, k1))
-            if v2 + 1 < 0:
-                heapq.heappush(heap, (v2 + 1, k2))
-        return len(nums) - c
+        n = len(nums)
+        i = nums[n // 2]
+        cnt = bisect_right(nums, i) - bisect_left(nums, i)
+        return max(n % 2, 2 * cnt - n)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
