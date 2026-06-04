01f59777be855b4d5bd9e83ff3ae426d409d3a7c
huan186 <131932368+huan186@users.noreply.github.com>
2026-06-04 08:56:29 +0700

    Time: 334 ms (55.56%), Space: 19.5 MB (14.56%) - LeetHub - Time: O(?), Space: O(?)

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -1,12 +1,17 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
         def waviness(n):
-            digits = list(map(int, str(n)))
-            return sum(
-                (digits[i] - digits[i - 1]) * (digits[i] - digits[i + 1]) > 0
-                for i in range(1, len(digits) - 1)
-            )
-        return sum(waviness(i) for i in range(num1, num2 + 1))
+            digits = []
+            while n > 0:
+                digits.append(n % 10)
+                n //= 10
+            w = 0
+            for i in range(1, len(digits) - 1):
+                if (digits[i] - digits[i - 1]) * (digits[i] - digits[i + 1]) > 0:
+                    w += 1
+            return w
+
+        return sum(waviness(i) for i in range(max(101, num1), num2 + 1))
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
