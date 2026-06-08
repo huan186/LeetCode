a4b51acdd885496126b53c62d21f7af27ab79462
huan186 <131932368+huan186@users.noreply.github.com>
2026-06-08 17:35:45 +0700

    Time: 27 ms (99.93%), Space: 42.9 MB (63.08%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        i, j = 0, 1
+        for num in nums:
+            if num > 0:
+                res[i] = num
+                i += 2
+            else:
+                res[j] = num
+                j += 2
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
