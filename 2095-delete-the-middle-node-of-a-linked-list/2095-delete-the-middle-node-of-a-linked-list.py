7ede36aeb0a60bb21969bb94294aafc2c33c34d7
huan186 <131932368+huan186@users.noreply.github.com>
2026-06-15 11:10:24 +0700

    Time: 80 ms (77.11%), Space: 62.9 MB (11.9%) - LeetHub - Time: O(n), Space: O(1)

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -1,8 +1,5 @@
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head.next:
-            return None
-
         dummy = ListNode(0, head)
         slow = dummy
         fast = head
