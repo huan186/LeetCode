617c280b0eae66388ac7565c87ae77d2e79ca850
huan186 <131932368+huan186@users.noreply.github.com>
2026-06-28 22:58:42 +0700

    Time: 11 ms (95.74%), Space: 28.2 MB (56.03%) - LeetHub - Time: O, Space: O(?)

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         arr.sort()
-        res = 1
-        for i, num in enumerate(arr):
-            res = min(num, res + 1, i + 1)
+        res = 0
+        for x in arr:
+            res = min(x, res + 1)
         return res
 
 # Synced seamlessly with LeetHub Pro
