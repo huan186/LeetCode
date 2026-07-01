dcc908789b0fa4f0b234bc41cbce4e2ee3e1df9c
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-01 09:50:12 +0700

    Time: 185 ms (9.13%), Space: 19.6 MB (14.05%) - LeetHub - Time: O(n), Space: O(1)

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,17 +1,20 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        res = 0
-        count = [0] * 3
+        cnt = [0] * 3
         l = 0
-        x = [ord(c) - ord('a') for c in s]
-        for i in x:
-            count[i] += 1
-            if all(c > 0 for c in count):
-                while count[x[l]] > 1:
-                    count[x[l]] -= 1
+        ans = 0
+
+        for r, ch in enumerate(s):
+            cnt[ord(ch) - ord('a')] += 1
+
+            if all(c > 0 for c in cnt):
+                while cnt[ord(s[l]) - ord('a')] > 1:
+                    cnt[ord(s[l]) - ord('a')] -= 1
                     l += 1
-                res += l + 1
-        return res
+
+                ans += l + 1
+
+        return ans
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
