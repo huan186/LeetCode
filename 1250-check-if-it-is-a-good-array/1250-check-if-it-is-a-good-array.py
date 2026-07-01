bf0dbf1d8cf1766022e630d9af1e782e45456f95
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-01 16:00:38 +0700

    Time: 5 ms (59.56%), Space: 29.6 MB (32.24%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isGoodArray(self, nums: List[int]) -> bool:
+        return reduce(gcd, nums) == 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
