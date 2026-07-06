d872516154a4127af654add72485390b6e7b69c1
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-06 10:26:52 +0700

    Time: 0 ms (100%), Space: 19.5 MB (96.95%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        end = 0
+        res = 0
+        for l, r in intervals:
+            if r > end:
+               res += 1
+               end = r
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
