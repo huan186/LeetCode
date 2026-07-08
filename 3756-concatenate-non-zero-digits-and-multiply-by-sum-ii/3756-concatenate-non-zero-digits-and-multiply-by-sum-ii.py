3dd2169c1914b780200ebd389528ef2bf89e943d
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-08 16:51:29 +0700

    Time: 519 ms (39.51%), Space: 61.7 MB (8.64%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
         mod = 10 ** 9 + 7
-        a, b, c = [0], [0], [0]
-        for ch in s:
-            if ch == '0':
-                a.append(a[-1])
-                b.append(b[-1])
-                c.append(c[-1])
-            else:
-                d = int(ch)
-                a.append((a[-1] * 10 + d) % mod)
-                b.append(b[-1] + d)
-                c.append(c[-1] + 1)
+        a = [(0, 0, 0)]
+        for c in s:
+            d = int(c)
+            a.append((
+                (a[-1][0] * 10 + d) % mod if d else a[-1][0],
+                a[-1][1] + d,
+                a[-1][2] + (d != 0),
+            ))
         res = []
         for l, r in queries:
-            x = b[r + 1] - b[l]
-            y = (a[r + 1] - a[l] * pow(10, c[r + 1] - c[l], mod)) % mod
+            x = a[r + 1][1] - a[l][1]
+            y = (a[r + 1][0] - a[l][0] * pow(10, a[r + 1][2] - a[l][2], mod)) % mod
             res.append((x * y) % mod)
         return res
 
