af943423ed0c4dbf123244778ee722fc63a61c4f
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-10 14:55:37 +0700

    Time: 71 ms (100%), Space: 24.9 MB (34.29%) - LeetHub - Time: O(n), Space: O(1)

--- 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.py
+++ 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.py
@@ -1,23 +1,16 @@
 class Solution:
     def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        while l <= r and nums[l] != 1:
-            l += 1
-        if l > r:
-            return 0
-        while l <= r and nums[r] != 1:
-            r -= 1
-        if l == r:
-            return 1
-        mod = 10 ** 9 + 7
+        MOD = 10 ** 9 + 7
+        prev = -1
         res = 1
-        while l < r:
-            i = l + 1
-            while i <= r and nums[i] != 1:
-                i += 1
-            res = (res * (i - l)) % mod
-            l = i
-        return res
+
+        for i, x in enumerate(nums):
+            if x:
+                if prev != -1:
+                    res = res * (i - prev) % MOD
+                prev = i
+
+        return res if prev != -1 else 0
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
