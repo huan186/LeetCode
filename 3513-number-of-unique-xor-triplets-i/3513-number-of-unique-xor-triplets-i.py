ce52c97f12f6743e6eda374efaf3996064d49c2b
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-23 10:17:26 +0700

    Time: 0 ms (100%), Space: 34.1 MB (27.12%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3: return n
+        return 1 << n.bit_length()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
