2efe389123b7268cc1c25f10e83b12a62d1f62c2
huan186 <131932368+huan186@users.noreply.github.com>
2026-07-31 09:37:39 +0700

    Time: 107 ms (52.17%), Space: 20.1 MB (23.08%) - LeetHub - Time: O(N), Space: O(1)

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -2,3 +2,7 @@ class Solution:
     def minimumPushes(self, word: str) -> int:
         f = sorted(Counter(word).values(), reverse=True)
         return sum(f[i] * (i // 8 + 1) for i in range(len(f)))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
