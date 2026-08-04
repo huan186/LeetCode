0c033747bd6cecd9adae107f3446ba9430b2eb93
huan186 <131932368+huan186@users.noreply.github.com>
2026-08-04 17:45:33 +0700

    Time: 3 ms (79.54%), Space: 20.4 MB (76.57%) - LeetHub - Time: O(n log n), Space: O(1)

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,14 +1,11 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        total = sum(nums)
-        s = 0
-        res = inf
-        n = len(nums)
-        for i in range(n):
-            s += nums[i]
-            res = min(res, nums[i] * (2 * i + 2 - n) + (total - 2 * s))
-        return res
+        ops = 0 
+        median = nums[len(nums)//2] 
+        for i in nums: 
+            ops += abs(i-median) 
+        return ops
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
