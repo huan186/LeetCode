784937865590d9f7798b1f5aedccbcdd82bbb0cb
huan186 <131932368+huan186@users.noreply.github.com>
2026-08-20 10:28:08 +0700

    Time: 0 ms (100%), Space: 19.3 MB (22.76%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -8,3 +8,7 @@ class Solution:
             else:
                 arr2.append(num)
         return arr1 + arr2
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
