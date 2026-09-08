681e3cb038301e94a326002070e7203b0cfd3b3d
huan186 <131932368+huan186@users.noreply.github.com>
2026-09-08 09:46:17 +0700

    Time: 0 ms (100%), Space: 19.3 MB (46.53%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        return 0 if n < 1000 else n - 999
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
