b6f12811948db3824cb6b3d958afd2cbc2cb43ac
huan186 <131932368+huan186@users.noreply.github.com>
2026-09-09 10:27:20 +0700

    Time: 0 ms (100%), Space: 19.3 MB (13.61%) - LeetHub - Time: O(log n), Space: O(1)

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,18 +1,11 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        a = 1
-        b = 1000
-        commas = 0
-        res = 0
-        while n >= a:
-            if n < b:
-                res += (n - a + 1) * commas
-                break
-            else:
-                res += (b - a) * commas
-                a, b = b, b * 1000
-                commas += 1
-        return res
+        ans = 0
+        x = 1000
+        while x <= n:
+            ans += n - x + 1
+            x *= 1000
+        return ans
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
